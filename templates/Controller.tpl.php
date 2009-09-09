4139c431e26616d42d24ca8ecb8d0552b42cd7e4
Brett Bieber <brett.bieber@gmail.com>
2009-09-09 14:18:34 +0000

    Setup sharedcode files.

--- templates/Controller.tpl.php
+++ templates/Controller.tpl.php
@@ -1,5 +1,12 @@
 <?php
-UNL_Templates::$options['version'] = 3;
+UNL_Templates::$options['version']        = 3;
+UNL_Templates::$options['sharedcodepath'] = dirname(__FILE__).'/sharedcode';
+
 $page = UNL_Templates::factory('Fixed');
+
+$page->doctitle     = '<title>UNL | Undergraudate Bulletin</title>';
+$page->titlegraphic = '<h1>2010-2011 Undergraduate Bulletin</h1>';
+
+
+$page->loadSharedCodeFiles();
 echo $page;
-?>
