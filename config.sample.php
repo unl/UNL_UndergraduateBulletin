f8b101de917ad83ea41e04572086fb7459c001dc
Brett Bieber <brett.bieber@gmail.com>
2009-09-09 14:47:54 +0000

    Add includes directory with savant.

--- config.sample.php
+++ config.sample.php
@@ -1,3 +1,3 @@
 <?php
-
+set_include_path(dirname(__FILE__).'/includes/php'.PATH_SEPARATOR.get_include_path());
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+
+if (file_exists('config.inc.php')) {
+    include_once 'config.inc.php';
+} else {
+    include_once 'config.sample.php';
+}
+
 require_once 'UNL/Autoload.php';
 
 $controller = new UNL_UndergraduateBulletin_Controller();
