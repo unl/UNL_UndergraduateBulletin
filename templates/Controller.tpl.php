5b3f4532e73dbc70bec308caab324c35663a0fcf
Brett Bieber <brett.bieber@gmail.com>
2009-09-09 18:22:26 +0000

    Add CSS files.

--- templates/Controller.tpl.php
+++ templates/Controller.tpl.php
@@ -3,7 +3,6 @@ UNL_Templates::$options['version']        = 3;
 UNL_Templates::$options['sharedcodepath'] = dirname(__FILE__).'/sharedcode';
 
 $page = UNL_Templates::factory('Fixed');
-
 $page->doctitle     = '<title>UNL | Undergraduate Bulletin</title>';
 $page->titlegraphic = '<h1>Undergraduate Bulletin 2010-2011</h1>';
 $page->navlinks     = '
@@ -12,6 +11,7 @@ $page->navlinks     = '
     <li><a href="?view=major">Major</a></li>
 </ul>';
 $page->loadSharedCodeFiles();
+$page->addStylesheet('templates/all.css');
 
 $page->maincontentarea = UNL_UndergraduateBulletin_OutputController::display($this->output, true);
 
