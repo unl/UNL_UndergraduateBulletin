8dd89af223d7ba4366404766df1d9c31933ee6b9
Brett Bieber <brett.bieber@gmail.com>
2009-09-10 00:31:29 +0000

    Make introduction look more like the mockup.

--- templates/Introduction.tpl.php
+++ templates/Introduction.tpl.php
@@ -1,2 +1,35 @@
-Welcome to the 2010-2011 Undergraduate Bulletin.
-This is the introduction.
+<div class="three_col left"
+    <h1>What you can find here:</h1>
+    <p>The Undergraduate Bulletin contains information about all programs of undergraduate study at the University of Nebraska&ndash;Lincoln.</p>
+    <div class="col left">
+        <h3 class="sec_main">College Information</h3>
+        <ul>
+            <li>Agricultural Sciences &amp; Natural Resources</li>
+            <li>Architecture</li>
+            <li>Arts &amp; Sciences</li>
+            <li>Business Adminsitration</li>
+            <li>Division of General Studies</li>
+            <li>Education &amp; Human Sciences</li>
+            <li>Engineering</li>
+            <li>Hixson-Lied College of Fine &amp; Performing Arts</li>
+            <li>Journalism &amp; Mass Communications</li>
+            <li>Public Affairs &amp; Community Service</li>
+        </ul>
+    </div>
+    <div class="col right">
+        <h3 class="sec_main">Academic Policies</h3>
+        <ul>
+            <li>...</li>
+        </ul>
+    </div>
+</div>
+<div class="col right">
+    <div class="zenbox ">
+        <h3>Find a Course</h3>
+        <input type="text" name="q" />
+    </div>
+    <div class="zenbox">
+        <h3>Areas of Study</h3>
+        <input type="text" name="q" />
+    </div>
+</div>
