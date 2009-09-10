2391e95f6210ce767974093180b0a9684f8f8113
Seth Meranda <smeranda2@unl.edu>
2009-09-10 21:07:50 +0000

    CSS and JS updates to the table of contents.

--- templates/scripts/majors.js
+++ templates/scripts/majors.js
@@ -17,7 +17,7 @@ wraphandler.addEvent(window,"load", function() {
     WDN.jQuery("#toc").tableOfContents(
     		WDN.jQuery("#long_content"),      // Scoped to div#long_content
       {
-        startLevel: 1,    // H1 and up
+        startLevel: 2,    // H1 and up
         depth:      5,    // H1 through H6,
         topLinks:   false, // Add "Top" Links to Each Header
       }
