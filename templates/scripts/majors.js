71d77d734e7577d2f60286f04d3cf77a073ec445
Brett Bieber <brett.bieber@gmail.com>
2009-09-10 20:53:26 +0000

    Fix a stray horizontal ellipses. Hide the nav when you click inside the ol

--- templates/scripts/majors.js
+++ templates/scripts/majors.js
@@ -1,11 +1,18 @@
 
 wraphandler.addEvent(window,"load", function() {
-    WDN.jQuery("#major_nav").hover(function() {
-        WDN.jQuery("#major_nav ol").show();
-    },
-    function() {
-        WDN.jQuery("#major_nav ol").hide();
-    });
+	WDN.jQuery("#major_nav ol").click(
+		function() {
+			WDN.jQuery("#major_nav ol").hide();
+		}
+	);
+    WDN.jQuery("#major_nav").hover(
+		function() {
+	        WDN.jQuery("#major_nav ol").show();
+	    },
+	    function() {
+	        WDN.jQuery("#major_nav ol").hide();
+	    }
+    );
     WDN.jQuery("#major_nav ol").hide();
     WDN.jQuery("#toc").tableOfContents(
     		WDN.jQuery("#long_content"),      // Scoped to div#long_content
