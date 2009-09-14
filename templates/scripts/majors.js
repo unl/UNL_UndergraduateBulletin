188217fec973320fd768605fb2c271f68e320557
Seth Meranda <smeranda2@unl.edu>
2009-09-14 20:14:48 +0000

    stop exectution of the a in .minimize.

--- templates/scripts/majors.js
+++ templates/scripts/majors.js
@@ -22,16 +22,17 @@ wraphandler.addEvent(window,"load", function() {
         topLinks:   false, // Add "Top" Links to Each Header
       }
     );
-    //End Deal with the Table of Contents for the majors pages.
+    //End: Deal with the Table of Contents for the majors pages.
     //
     //Deal with the interactivity behind the wdn_notice
     WDN.jQuery(".minimize").click(function() {
     	WDN.jQuery(this).parent(".wdn_notice").slideUp("slow", function() {
-    			WDN.jQuery(this).wrap("<div class='col right'></div>"); //wrap in a col, floated right
+    			WDN.jQuery(this).wrap("<div class='col right' id='wdn_notice_wrapper'></div>"); //wrap in a col, floated right
     			WDN.jQuery(this).children("div.message").children("p").children("a").insertAfter("div.message p").siblings("p").hide();
     			WDN.jQuery(this).children(".minimize").removeClass("minimize").addClass("maximize");
     			WDN.jQuery(this).slideDown("slow");
     	});
+    	return false;
     });
-    
+    //End: Deal with the interactivity behind the wdn_notice
 });
