3ed7b119f5582d35f0e023434d7bb3756f9a02b8
Brett Bieber <brett.bieber@gmail.com>
2009-09-17 20:32:29 +0000

    Complete social science example with college requirements.

--- data/samples/SocialScience.php
+++ data/samples/SocialScience.php
@@ -5,17 +5,114 @@
 $major->title = 'Social Science [Grades 7-12] Endorsement';
 $major->college = 'College of Education and Human Sciences';
 $major->description->degrees_offered[] = 'Bachelor of Science in Education and Human Sciences';
-$major->description->hours_required = 125;
-$major->description->minor_available = false;
+$major->description->hours_required = '125-136';
+$major->description->minor_available = true;
 $major->description->chief_advisor = 'Dr. Beverly Jensen';
 $major->description->description = <<<MAJOR
 <h2 class="sec_header">Description</h2>
 <p>Students who complete this endorsement are certified to teach history, anthropology, economics, geography, political science, psychology, and sociology in GRADES 7-12.</p>
+<h3>Degree At A Glance:</h3> 
+<pre>
+ACE........................................12 - 30 hours
+Pre-Professional Education Courses.........13 hours
+Social Sciences 7-12 Endorsement...........62-63 hours
+Professional Education Courses.............30 hours *
+Electives...................................0 - 8 hours
+TOTAL HOURS:  125 - 136
+</pre>
+<p>
+Note: Total hours varies based upon the courses students choose to complete for their ACE requirement.
+</p>
+<p>
+* Faculty need to determine what course will fulfill ACE 10.  If it is the portfolio which is done in the 1 credit seminar, the professional education courses will increase from 28 to 30 hours.
+</p>
 MAJOR;
 
 $major->description->admission = <<<ADMISSION
 <h2 class="sec_header">Admission</h2>
+<h3>College Admission</h3>
+<p>
+Students accepted by the University must have an ACT of 20 or SAT of 950, or rank in the upper
+half of their high school graduating class, and have the following high school preparation to be eligible
+for admission to the College of Education and Human Sciences.
+</p>  
+<ul>
+<li>four years of English that include intensive reading and writing experience;</li>  
+<li>two years of one foreign language; </li>
+<li>four years of mathematics, that include Algebra I, II, geometry and one year that builds on a knowledge of algebra; </li>
+<li>three years of natural sciences that include at least two years selected from biology, physics, chemistry, and earth science and one year of laboratory instruction;</li>
+<li>three years of social studies, that include at least one year of American and/or world history and one year of history, American government, and/or geography.</li>
+</ul>
+<p>
+Transfer students from universities or colleges outside of UNL and readmitted students seeking admission to the College of Education and Human Sciences must have an accumulated average of 2.0 on a 4.0 scale or above and no high school deficiencies.
+Students who do not meet these requirements must enroll as deciding students in the Division of General Studies or in another college. Once they have completed 12 graded hours at UNL with a minimum 2.0 grade point average, and have removed any high school deficiencies, UNL students may apply for admission to the College.
+</p>
+<p>
+The College of Education and Human Sciences welcomes undergraduate international students.  As a part of admission to the College, international students must present a TOEFL score of 550 or higher, and TSE score of 230 or higher.
+</p>
+<p>
+ Students transferring to the College of Education and Human Sciences from another University of Nebraska&ndash;Lincoln college or from the Division of General Studies must have a minimum cumulative GPA of 2.0, be in good academic standing, and meet the freshman entrance requirements that exist at the time of their admission to the College of Education and Human Sciences.
+</p>
+<p>
+The College of Education and Human Sciences welcomes undergraduate international students.<br />
+As a part of admission to the College, international students must present a TOEFL score of 550 or higher, and TSE score of 230 or higher.  Students seeking teacher education and state certification must meet the same requirements as any other undergraduate students, including the Pre-Professional Skills Test or other basic skills test approved by the Nebraska Department of Education.
+Students who have received a degree outside of the United States and are interested in teacher certification are required to have a transcript review completed by an approved agency not directly associated with the University of Nebraska. See the director of advising in the College Student Services Center for details.
+<p>
+To remain current, College of Education and Human Sciences students must enroll in, and  complete, at least one UNL course that will apply toward degree requirements during a 12 month period. Students who readmit following an absence of one year or more must meet all requirements in the undergraduate bulletin in effect at the time of readmission and enrollment. Students who transfer to another UNL college and later return to the College of Education and Human Sciences will be considered readmitted students.
+<p>
+Students who transfer out of a teacher education program, but who continue their certification program while seeking a degree in another UNL college, are exempt from this policy.
+
+<h3>Major/Department Admission</h3>
 <p>The Social Science 7-12 endorsement has competitive admission.  Students must apply to be admitted into the endorsement.  An interview is required.</p>
+
+<h3>Admission Deficiencies</h3> 
+<p>All admission deficiencies must be removed prior to admission to the College.</p>  
+
+<p>Students admitted to the University with three or more high school deficiencies, or two deficiencies in a single category other than foreign language, will not be admitted to a teacher education program until such deficiencies are removed.
+</p>
+<h3>Other Admission Requirements</h3>
+
+<p>FOLLOWING ON ALL TEACHER EDUCATION ENDORSEMENTS BUT NOT ON HUMAN SCIENCES MAJORS.</p>
+<h4>Pre-Education</h4>
+<p>All newly admitted students who are pursuing a teacher preparation program, except those in nonteaching majors, are classified as pre-education until admitted to a Teacher Education Program.
+
+<h4>Admission to a Teacher Education<br />
+Program (TEP)</h4>
+<p>Admission to the College of Education and Human Sciences does not guarantee admission to a teacher education program. Admission to the advanced phases of teacher education is selective and, in some endorsements, highly competitive.   Selection to a TEP is based upon the following criteria:</p>
+<ol>
+<li>Elementary education students must earn a minimum of 30 credit hours of college credit;  secondary education students must earn a
+minimum of 42 credit hours of college credit.</li>
+<li>Completion and submission of the TeacherEducation Program Approval Form.</li>
+<li>Completion of TEAC 331 or 430 or 431 or 434 or 437 or 496 (3 hrs) or approved transfer course, and EDPS 250 or 251 with a 2.5 cumulative average in the two classes, and no grade lower than C.</li>
+<li>Documentation of proficiency in reading, writing, and mathematics through successful completion of a basic skills examination that
+meets the Nebraska Department of Education competency requirement.</li>
+<li>Completion of one course in communication studies selected from COMM 109, 205, 209, 210, or 286, or approved substitute.</li>
+<li>Faculty recommendations.</li>
+<li>Completion of a personal and professional fitness self-disclosure form as well as the completion of a formal criminal history review. (See Felony and Misdemeanor Convictions under OTHER.)</li>
+<li>Specific programs may have particular learning outcomes that students must address as part of the application process.</li>
+</ol>
+
+<h4>Post-baccalaureate Students Seeking
+Initial Teaching Certification</h4>
+<p>Students who have received a bachelors degree or higher and desire to obtain an initial teaching certificate may do so by pursuing a non-degree post-baccalaureate initial certification program with or without a masters degree. They must apply to the Graduate College for admission to the University of Nebraska–Lincoln and apply to a Teacher Education Program (TEP) for admission to the initial teaching certification program. Students are also required to apply for admission to a degree program if they desire to pursue a masters
+degree with their certification program. All students seeking initial certification must meet with the post baccalaureate assistant academic adviser.  Those seeking a masters degree will also meet with a faculty adviser. Post-baccalaureate students
+interested in completing a teacher education program should refer to this bulletin as to policies and regulations for program completion. For specific guidelines, see the Student Services Center.
+</p>
+
+<h4>Admission to Student Teaching</h4>
+<p>All students who are candidates for an appropriately endorsed Nebraska Teacher’s Certificate are required to student teach. Students who plan to student teach in the fall semester must complete the student teaching application form and submit it by the preceding March 1 to the Director of Field Experiences in 104 Henzlik Hall; students planning to student teach in the spring semester must
+apply by the preceding October 1. The basic program for student teaching provides for a full-day experience on a semester basis. Students enrolled in an elementary education dual major will complete requirements for student teaching in both majors. Admission to student teaching requires the following:
+</p>
+<ol>
+<li>Matriculation in a teacher education program in the College of Education and Human Sciences, the Graduate College, or dual matriculation in the College of Education and Human Sciences and another college.</li>
+<li>Admission to a teacher education program.</li>
+<li>Senior standing (89 hours or more) with a minimum cumulative GPA of 2.5.</li>
+<li>Minimum average of 2.5 in each endorsement area with no grade below C.</li>
+<li>A minimum grade point average of 2.5 in preprofessional  nd professional education courses
+and no grade below C in pre-professional education courses and no grade below a C+ in professional education courses.</li>
+<li>Completion of a criminal history check that will be conducted by an independent party (fee required).</li>
+</ol>
+
 ADMISSION;
 $major->description->major_requirements = <<<REQ
 <h2 class="sec_header">Major Requirements</h2>
@@ -247,23 +344,93 @@ REQ;
 
 $major->description->additional_major_requirements = <<<ADDITIONAL
 <h2 class="sec_header">Additional Major Requirements</h2>
-<h3>Grade Rules (Pass/No Pass, Specific Grade Rules related to the major)</h3>
-<p>
-For students in certification programs, preprofessional and professional education classes
-may not be taken Pass/No Pass unless a class already has a Pass/No Pass designation.  Only one course in each subject endorsement;  two courses in a field endorsement.
-</p>
-<p>
-Requirements for completion of an undergraduate degree in a teacher preparation program
-include a minimum cumulative grade point average (GPA) of 2.5. Students seeking certification
-must also have a 2.5 GPA in their education courses with no grade lower than a C. Students are
-also required to have a 2.5 GPA in their subject endorsement area(s) with no grade lower than C in
-either area.  Grades below C (C-, D+, D and D-) may not be applied in any endorsement, professional education requirement or non-teaching major professional course requirement in degrees leading to teacher preparation.  
-</p>
+&bull; Pass/No Pass<br />
+For students in certification programs, preprofessional and professional education classes may not be taken Pass/No Pass unless a class already has a Pass/No Pass designation.  Only one course in each subject endorsement;  two courses in a field endorsement.<br />  
+&bull; C- and D Grades<br />
+Grades below C (C-, D+, D and D-) may not be applied in any endorsement, professional education requirement or non-teaching major professional course requirement in degrees leading to teacher preparation.  <br />
+&bull; GPA Requirements<br />
+Requirements for completion of an undergraduate degree in a teacher preparation program include a minimum cumulative grade point average (GPA) of 2.5. Students seeking certification must also have a 2.5 GPA in their education courses with no grade lower than a C. Students are also required to have a 2.5 GPA in their subject endorsement area(s) with no grade lower than C in either area.<br />  
+
 ADDITIONAL;
 
 $major->description->college_degree_requirements = <<<COLLEGE
+<h2 class="sec_header">College Degree Requirements</h2>
+<h3>College General Education Requirements </h3>
+<p>The goals of a general, liberal education are to promote the understanding of broad areas of knowledge and to develop attitudes, values, thought processes, and basic abilities expected from an educated person. All UNL students who are following the 2009-10 bulletin will be required to complete a minimum of 3 hours of approved course work in each of the 10 designated ACE Student Learning Outcome (SLO) areas which are described on page at ace.unl.edu.   It is highly recommended that students contact their adviser prior to registering for ACE classes in order to insure that each of the class selections are in the best interest of the students’ academic program.</p>
+
+<h4>Additional ACE Course Work For Secondary Education</h4>
+<p>Students need four additional ACE courses.<br />
+One of the 14 total ACE courses must be an approved speech course: COMM 109, 205, 209, 210. At least two of the 14 ACE courses must be at the 300 or 400 level.</p>
+
+<h3>Grade Rules</h3>
+<h4>C- and D Grades</h4>
+<p> 
+Grades of D-, D, D+, and C- satisfy requirements in all programs in the College.
+<p>
+Up to 9 hours of transfer credit with grades below C may be applied to the General Education requirements and elective classes in programs leading to the undergraduate degree in teacher
+preparation. Transfer grades of C- and D may not be used in the major/endorsement area courses or in the professional courses in teacher preparation programs.
+
+<h4>Pass/No Pass Limits</h4>
+<p>A student enrolled at the University may, in certain instances, take a grading option of Pass/No
+Pass (P/N) for a specific course. A grade of pass represents satisfactory completion of a course with a
+grade of C or better. Credits earned under the pass grade option count toward graduation, but no grade
+points are tabulated in the cumulative grade point average. Likewise, a grade of no pass is not tabulated in the grade point average. The following rules apply to students who are enrolled in teacher preparation programs who choose the Pass/No Pass option:
+</p>
+<ol>
+<li>Only one course in each subject endorsement;  two courses in a field endorsement.</li>
+<li>For students in certification programs, preprofessional and professional education classes
+may not be taken Pass/No Pass unless a class already has a Pass/No Pass designation</li>
+<li>Any course in the general education requirements unless otherwise stipulated by the
+department of the course.</li> 
+<li>Total P/N credits may not exceed 12 credit hours. This limit does not include courses
+offered on a Pass/No Pass only basis.</li>
+</ol>
+
+<h3>Transfer Credit</h3>
+<strong>Grades Earned Outside UN System</strong><br />
+<p>
+The college will accept no more than 9 credit hours of grades less than a C from any program
+outside the University of Nebraska system. Grades below a C can only be applied to general education
+requirements and elective classes.
+</p>
+<p>
+Up to 9 hours of transfer credit with grades below C may be applied to the General Education
+requirements and elective classes in programs leading to the undergraduate degree in teacher
+preparation. Transfer grades of C- and D may not be used in the major/endorsement area courses or
+in the professional courses in teacher preparation programs.
+</p>
+<p>
+Transfer courses are evaluated by the University and the College to determine UNL and College course equivalencies. The College determines which courses will be accepted and how they will apply toward degree requirements. Sixty-six (66) is the maximum number of hours that will be accepted on transfer from a two-year college.  Ninety-five (95) is the maximum number of hours that will be accepted on transfer from accredited four-year colleges and universities.  Courses taken 10 years before admission or readmission to the College will be evaluated by the major department to determine if it is appropriate
+to accept those courses for transfer and application to degree requirements. Specific courses will be reviewed in keeping with the guidelines specified by each department.
+</p>
+
+<h3>Residency</h3>
+<p>
+A minimum total of 125 credit hours of course work in teacher preparation programs is required
+for a bachelors degree in education and human sciences, of which 30 of the last 36 must be taken in
+residence at UNL. Independent Study and summer reading courses sponsored by the UNL
+Extended Education do not apply to residency.  Credit earned during study abroad may be used toward degree requirements if students participate in prior approved programs and register through UNL.
+</p>
+
+<h3>Extended Education, Independent Study Rules, Internship Credit Rules</h3>
+<p>Independent Study and summer reading courses sponsored by the UNL Extended Education do not apply to residency.</p>  
+
+<h3>Bulletin Rule</h3>
+<p>Students must fulfill degree requirements that exist at the time of their admission to the college, not at the time they enter UNL.<br />
+Transfer and readmitted students must meet the graduation requirements for the College of Education and Human Sciences as stated in the current catalog in effect at the time they enter or reenter the College.
+</p>
+
+
 COLLEGE;
 $major->description->ace_requirements = <<<ACE
+<h2 class="sec_header">ACE Requirements</h2>
+
+<p>All students must fulfill the Achievement Centered Education (ACE) requirements.  Information about the ACE program may be viewed at ace.unl.edu.</p>
+<h4>Additional ACE Course Work For Secondary Education</h4>
+Students need four additional ACE courses.<br />
+One of the 14 total ACE courses must be an approved speech course: COMM 109, 205, 209,
+210. At least two of the 14 ACE courses must be at the 300 or 400 level.<br />
+
 ACE;
 $major->description->other = <<<OTHER
 <h2 class="sec_header">Other</h2>
