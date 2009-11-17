8dd73480cd2411f2133cde8d7b5e2533afb990a2
Brett Bieber <brett.bieber@gmail.com>
2009-11-17 14:13:30 +0000

    Correct courseGroup parsing, they are not a CSV field. Add form for filtering options.

--- templates/SubjectArea.tpl.php
+++ templates/SubjectArea.tpl.php
@@ -1,11 +1,29 @@
 <?php
 echo '<h1>'.htmlentities($this->subject).'</h1>';
 
-echo  '<dl>';
+echo  '<div class="three_col left">
+    <dl>';
 
 foreach ($this->courses as $course) {
     include dirname(__FILE__).'/Course.tpl.php';
 }
-echo  '</dl>';
-
+echo  '</dl></div>';
 ?>
+<div class="col right zenbox">
+    <h3>Filter Options</h3>
+    <form method="POST">
+        <fieldset>
+            <legend>Groups</legend>
+            <ol>
+                <?php foreach ($this->groups as $key=>$group) : ?>
+                <li>
+                    <label><?php echo $group; ?></label>
+                    <div class="element">
+                        <input type="checkbox" value="grp_<?php echo $key; ?>" />
+                    </div>
+                </li>
+                <?php endforeach; ?>
+            </ol>
+        </fieldset>
+    </form>
+</div>
