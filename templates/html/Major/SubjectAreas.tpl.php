f2d67a8e50f93bc0e6ea0fc66c0e64b740a72874
Seth Meranda <smeranda2@unl.edu>
2010-01-25 16:44:31 +0000

    Filter box styling, updates

--- templates/html/Major/SubjectAreas.tpl.php
+++ templates/html/Major/SubjectAreas.tpl.php
@@ -1,8 +1,5 @@
-<div class="four_col">
-<h2>Courses of Instruction</h2>
 <?php
 foreach ($this->major->subjectareas as $subject) {
     UNL_UndergraduateBulletin_OutputController::display($subject);
 }
 ?>
-</div>
--- templates/html/SubjectArea.tpl.php
+++ templates/html/SubjectArea.tpl.php
@@ -1,58 +1,60 @@
 <?php
-echo '<h1>'.htmlentities($this->subject).'</h1>';
+echo '<h2 class="sec_main"> Courses of Instruction ('.htmlentities($this->subject).')</h2>';
  ?>
-<div class="col left zenbox energetic" id="displayControl">
-    <h3>Filter Options</h3>
-    <form method="POST" action="#" id="filters">
-        <?php if (count($this->groups)) : ?>
-        <fieldset>
-            <legend><span>Groups</span></legend>
-            <ol>
-                <?php foreach ($this->groups as $group) : ?>
-                <li>
-                    <div class="element">
-                        <input type="checkbox" checked="checked" value="grp_<?php echo md5($group); ?>" />
-                    </div>
-                    <label><?php echo $group; ?></label>
-                </li>
-                <?php endforeach; ?>
-            </ol>
-        </fieldset>
-        <?php endif; ?>
-        <fieldset>
-            <legend><span>Course Formats</span></legend>
-            <ol>
-                <?php foreach (array(
-'lec'=>'Lecture',
-'lab'=>'Lab',
-'quz'=>'Quiz',
-'rct'=>'Recitation',
-'stu'=>'Studio',
-'fld'=>'Field',
-'ind'=>'Independent Study',
-'psi'=>'Personalized System of Instruction') as $key=>$type) : ?>
-                <li>
-                    <div class="element">
-                        <input type="checkbox" checked="checked" value="<?php echo $key; ?>" />
-                    </div><label><?php echo $type; ?></label>
-                </li>
-                <?php endforeach; ?>
-            </ol>
-        </fieldset>
-        <fieldset>
-            <legend><span>Ace Outcomes</span></legend>
-            <ol>
-                <?php for ($i=1;$i<=10;$i++) : ?>
-                <li>
-                    <div class="element">
-                        <input type="checkbox" checked="checked" value="ace_<?php echo $i; ?>" />
-                    </div>
-                    <label><?php echo $i; ?></label>
-                </li>
-                <?php endfor; ?>
-            </ol>
-        </fieldset>
-    </form>
+ <div class="col left">
+	<div class="zenbox energetic" id="wdn_filterset">
+	    <h3>Filter these Courses</h3>
+	    <form method="POST" action="#" id="filters">
+	        <?php if (count($this->groups)) : ?>
+	        <fieldset>
+	            <legend><span>Groups</span></legend>
+	            <ol>
+	                <?php foreach ($this->groups as $group) : ?>
+	                <li>
+	                    <div class="element">
+	                        <input type="checkbox" checked="checked" value="grp_<?php echo md5($group); ?>" />
+	                    </div>
+	                    <label><?php echo $group; ?></label>
+	                </li>
+	                <?php endforeach; ?>
+	            </ol>
+	        </fieldset>
+	        <?php endif; ?>
+	        <fieldset>
+	            <legend><span>Course Formats</span></legend>
+	            <ol>
+	                <?php foreach (array(
+	'lec'=>'Lecture',
+	'lab'=>'Lab',
+	'quz'=>'Quiz',
+	'rct'=>'Recitation',
+	'stu'=>'Studio',
+	'fld'=>'Field',
+	'ind'=>'Independent Study',
+	'psi'=>'Personalized System of Instruction') as $key=>$type) : ?>
+	                <li>
+	                    <div class="element">
+	                        <input type="checkbox" checked="checked" value="<?php echo $key; ?>" />
+	                    </div><label><?php echo $type; ?></label>
+	                </li>
+	                <?php endforeach; ?>
+	            </ol>
+	        </fieldset>
+	        <fieldset>
+	            <legend><span>Ace Outcomes</span></legend>
+	            <ol>
+	                <?php for ($i=1;$i<=10;$i++) : ?>
+	                <li>
+	                    <div class="element">
+	                        <input type="checkbox" checked="checked" value="ace_<?php echo $i; ?>" />
+	                    </div>
+	                    <label><?php echo $i; ?></label>
+	                </li>
+	                <?php endfor; ?>
+	            </ol>
+	        </fieldset>
+	    </form>
+	</div>
 </div>
 <?php
 
