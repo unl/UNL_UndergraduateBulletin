cea1fb2143827b2a064164897cc32284692292dd
Brett Bieber <brett.bieber@gmail.com>
2010-02-22 17:25:43 +0000

    Fix link to major.

--- www/templates/html/Major.tpl.php
+++ www/templates/html/Major.tpl.php
@@ -7,6 +7,6 @@ UNL_UndergraduateBulletin_Controller::setReplacementData('head', '<script type="
 <h1><?php echo $context->title; ?></h1>
 <h2 class="subhead"><?php echo $context->college; ?></h2>
 <ul class="wdn_tabs">
-    <li><a href="<?php echo $url; ?>?view=major&amp;name=<?php echo urlencode($context->title); ?>"><span>Description</span></a></li>
+    <li><a href="<?php echo $url; ?>major/<?php echo urlencode($context->title); ?>"><span>Description</span></a></li>
     <li <?php echo ($_GET['view']=='courses')?'class="selected"':''; ?>><a href="<?php echo $url; ?>?view=courses&amp;name=<?php echo urlencode($context->title); ?>"><span>Courses</span></a></li>
 </ul>
