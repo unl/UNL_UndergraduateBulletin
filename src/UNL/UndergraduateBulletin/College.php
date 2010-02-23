6adfc527914d489c7e394560892d8f573e6de38c
Brett Bieber <brett.bieber@gmail.com>
2010-02-23 02:06:27 +0000

    Only pull the body content of the college pages. Apply the same styling changes to the college page.

--- src/UNL/UndergraduateBulletin/College.php
+++ src/UNL/UndergraduateBulletin/College.php
@@ -20,7 +20,22 @@ class UNL_UndergraduateBulletin_College
             throw new Exception('No description for the '.$name.' college.');
         }
         $file = 'phar://'.dirname(dirname(dirname(dirname(__FILE__)))).'/data/colleges/'.self::$files[$name];
-        $this->description = file_get_contents($file);
+        
+        $simplexml = simplexml_load_string(file_get_contents($file));
+        
+        // Fetch all namespaces
+        $namespaces = $simplexml->getNamespaces(true);
+        $simplexml->registerXPathNamespace('default', $namespaces['']);
+        
+        // Register the rest with their prefixes
+        foreach ($namespaces as $prefix => $ns) {
+            $simplexml->registerXPathNamespace($prefix, $ns);
+        }
+
+        $body = $simplexml->xpath('//default:body');
+
+        $this->description = UNL_UndergraduateBulletin_EPUB_Utilities::convertHeadings($body[0]->asXML());
+        $this->description = UNL_UndergraduateBulletin_EPUB_Utilities::addLeaders($this->description);
 
     }
 }
