2fb49ee9126d9f072b86572e75c6c0e11b0a89cc
Brett Bieber <brett.bieber@gmail.com>
2010-03-13 22:27:22 +0000

    Get simple subject code results working.

--- src/UNL/UndergraduateBulletin/CourseSearch.php
+++ src/UNL/UndergraduateBulletin/CourseSearch.php
@@ -7,7 +7,19 @@ class UNL_UndergraduateBulletin_CourseSearch
     function __construct($options = array())
     {
         $search = new UNL_Services_CourseApproval_Search();
-        $this->results = $search->byNumber($options['q']);
+        
+        $parts = explode(' ', $options['q']);
+        foreach ($parts as $part) {
+            switch(true) {
+                case preg_match('/([0-9]{2,3}[A-Z]?)/', $part):
+                    $this->results = $search->byNumber($part);
+                    break;
+                case preg_match('/([A-Z]{2,3})/i', $part):
+                    $this->results = $search->bySubject($part);
+                    break;
+            }
+        }
+        
     }
 }
 ?>
