f673f643140a2a98ca95896a18dea1f39d1db1a4
Brett Bieber <brett.bieber@gmail.com>
2010-03-14 01:48:52 +0000

    Set defaults for the limit.

--- src/UNL/UndergraduateBulletin/CourseSearch.php
+++ src/UNL/UndergraduateBulletin/CourseSearch.php
@@ -4,15 +4,17 @@ class UNL_UndergraduateBulletin_CourseSearch
 
     public $results;
     
-    public $options;
+    public $options = array('q'      => null,
+                            'offset' => 0,
+                            'limit'  => 15);
     
     function __construct($options = array())
     {
-        $this->options = $options;
+        $this->options = $options + $this->options;
 
         $search = new UNL_Services_CourseApproval_Search();
 
-        $this->results = $search->byAny($this->options['q']);
+        $this->results = $search->byAny($this->options['q'], $this->options['offset'], $this->options['limit']);
 
     }
 }
