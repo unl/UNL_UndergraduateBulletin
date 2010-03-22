23c58b1a218a28619c27c4e57690816666e6069c
Brett Bieber <brett.bieber@gmail.com>
2010-03-22 18:54:47 +0000

    whitespace and formatting

--- www/templates/html/SubjectArea.tpl.php
+++ www/templates/html/SubjectArea.tpl.php
@@ -3,15 +3,14 @@ echo '<h2 class="sec_main"> Courses of Instruction ('.$context->subject.')</h2>'
 echo '<a href="#" id="toggleAllCourseDescriptions">Hide all course descriptions</a>';
  ?>
  <div class="col left">
-	<?php echo $savvy->render($context, 'CourseFilters.tpl.php'); ?>
+    <?php echo $savvy->render($context, 'CourseFilters.tpl.php'); ?>
 </div>
-<?php
-
-echo  '<div class="three_col right">
-    <dl>';
-
-foreach ($context->courses as $course) {
-    echo $savvy->render($course);
-}
-echo  '</dl></div>';
-?>
+<div class="three_col right">
+    <dl>
+    <?php
+    foreach ($context->courses as $course) {
+        echo $savvy->render($course);
+    }
+    ?>
+    </dl>
+</div>
