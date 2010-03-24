247f638e6c3f09e7f0dc00a157e59e32a4a6b99b
Brett Bieber <brett.bieber@gmail.com>
2010-03-24 01:13:52 +0000

    Update snapshot of all course data.

--- data/creq/update.php
+++ data/creq/update.php
@@ -1,10 +1,10 @@
 <?php
 
-$subjects = file(dirname(__FILE__).'/subject_codes.csv', FILE_IGNORE_NEW_LINES);
-foreach ($subjects as $subject) {
-    if ($data = file_get_contents('http://creq.unl.edu/courses/public-view/all-courses/subject/'.$subject)) {
-        file_put_contents(dirname(__FILE__).'/subjects/'.$subject.'.xml', $data);
+$handle = fopen(dirname(__FILE__).'/subject_codes.csv', 'r');
+while (($subject = fgetcsv($handle, 1000, ",", "'")) !== false) {
+    if ($data = file_get_contents('http://creq.unl.edu/courses/public-view/all-courses/subject/'.$subject[0])) {
+        file_put_contents(dirname(__FILE__).'/subjects/'.$subject[0].'.xml', $data);
     } else {
-        echo 'Could not retrieve data for '.$subject.PHP_EOL;
+        echo 'Could not retrieve data for '.$subject[0].PHP_EOL;
     }
 }
--- data/creq/update.sh
+++ data/creq/update.sh
@@ -1,3 +1,3 @@
 wget http://creq.unl.edu/courses/public-view/all-courses -O all-courses.xml
-grep "<subject>" all-courses.xml | grep --regexp "[A-Z]\{3,4\}" -o | sort | uniq > subject_codes.csv
+#grep "<subject>" all-courses.xml | grep --regexp "[A-Z]\{3,4\}" -o | sort | uniq > subject_codes.csv
 php update.php
