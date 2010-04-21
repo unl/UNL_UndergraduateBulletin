0348ed6634e5b2d2618213254881dbb1549016c3
Brett Bieber <brett.bieber@gmail.com>
2010-04-21 13:54:05 +0000

    Add info about caching and setup to the README and config files.

--- config.sample.php
+++ config.sample.php
@@ -7,12 +7,16 @@ function autoload($class)
     
 spl_autoload_register("autoload");
 
+// Set to false on production machines
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
 set_include_path(dirname(__FILE__).'/src'.PATH_SEPARATOR.dirname(__FILE__).'/includes/php'.PATH_SEPARATOR.get_include_path());
 require_once 'UNL/Autoload.php';
-ini_set('display_errors', true);
+
+// Set this to the web root for the site
 UNL_UndergraduateBulletin_Controller::$url = '/workspace/UNL_UndergraduateBulletin/www/';
+
+// Remove this line on production machines so the default UNLCacheLite cache interface is used
 UNL_UndergraduateBulletin_OutputController::setCacheInterface(new UNL_UnderGraduateBulletin_CacheInterface_Mock());
 ?>
