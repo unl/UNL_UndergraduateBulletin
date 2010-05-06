6baceae0ac86976c4fe7b80da78f55ff20225487
Brett Bieber <brett.bieber@gmail.com>
2010-05-06 15:05:54 +0000

    Use new subjectarea object correctly.

--- www/templates/html/SubjectAreas.tpl.php
+++ www/templates/html/SubjectAreas.tpl.php
@@ -1,8 +1,8 @@
 <h2>All subject areas</h2>
 <ul id="subjectListing">
 <?php
-foreach ($context as $subject_code=>$title) {
-    echo '<li><a href="'.UNL_UndergraduateBulletin_Controller::getURL().'courses/'.$subject_code.'/"><span class="subjectCode">'.$subject_code.'</span> <span class="title">'.$title.'</span></a></li>';
+foreach ($context as $subject_code=>$area) {
+    echo '<li><a href="'.UNL_UndergraduateBulletin_Controller::getURL().'courses/'.$subject_code.'/"><span class="subjectCode">'.$subject_code.'</span> <span class="title">'.$area->title.'</span></a></li>';
 }
 ?>
 </ul>
