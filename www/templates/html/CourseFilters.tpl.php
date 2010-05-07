9df12ca0cd71853a914734a0dd46a62999f2a80f
Brett Bieber <brett.bieber@gmail.com>
2010-05-07 15:25:28 +0000

    Only prefix with subject code if it is set.

--- www/templates/html/CourseFilters.tpl.php
+++ www/templates/html/CourseFilters.tpl.php
@@ -1,3 +1,9 @@
+<?php
+$idPrefix = '';
+if (isset($context->subject)) {
+    $idPrefix = $context->subject;
+}
+?>
 <div class="zenbox energetic wdn_filterset">
     <h3>Filter these Courses</h3>
     <form method="post" action="#" id="<?php echo $context->subject; ?>_filters" class="filters">
@@ -7,14 +13,14 @@
             <legend><span>Groups</span></legend>
             <ol>
                 <li>
-                    <input type="checkbox" checked="checked" class="filterAll" id="<?php echo $context->subject; ?>_filterAllGroups" name="all" value="all" />
-                    <label for="<?php echo $context->subject; ?>_filterAllGroups">All groups</label></li>
+                    <input type="checkbox" checked="checked" class="filterAll" id="<?php echo $idPrefix; ?>_filterAllGroups" name="all" value="all" />
+                    <label for="<?php echo $idPrefix; ?>_filterAllGroups">All groups</label></li>
                 <?php foreach ($context->groups as $group) : ?>
                 <li>
                     <div class="element">
-                        <input type="checkbox" id="<?php echo $context->subject; ?>_filter_grp_<?php echo md5($group); ?>" value="grp_<?php echo md5($group); ?>" />
+                        <input type="checkbox" id="<?php echo $idPrefix; ?>_filter_grp_<?php echo md5($group); ?>" value="grp_<?php echo md5($group); ?>" />
                     </div>
-                    <label for="<?php echo $context->subject; ?>_filter_grp_<?php echo md5($group); ?>"><?php echo $group; ?></label>
+                    <label for="<?php echo $idPrefix; ?>_filter_grp_<?php echo md5($group); ?>"><?php echo $group; ?></label>
                 </li>
                 <?php endforeach; ?>
             </ol>
@@ -23,8 +29,8 @@
         <fieldset class="formats">
             <legend><span>Course Formats</span></legend>
             <ol>
-               <li><input type="checkbox" checked="checked" class="filterAll" id="<?php echo $context->subject; ?>_filterAllFormats" name="all" value="all" />
-               <label for="<?php echo $context->subject; ?>_filterAllFormats">All formats</label></li>
+               <li><input type="checkbox" checked="checked" class="filterAll" id="<?php echo $idPrefix; ?>_filterAllFormats" name="all" value="all" />
+               <label for="<?php echo $idPrefix; ?>_filterAllFormats">All formats</label></li>
                 <?php foreach (array(
 'lec'=>'Lecture',
 'lab'=>'Lab',
@@ -36,8 +42,8 @@
 'psi'=>'Personalized System of Instruction') as $key=>$type) : ?>
                 <li>
                     <div class="element">
-                        <input type="checkbox" id="<?php echo $context->subject; ?>_filter_format_<?php echo $key; ?>" value="<?php echo $key; ?>" />
-                    </div><label for="<?php echo $context->subject; ?>_filter_format_<?php echo $key; ?>"><?php echo $type; ?></label>
+                        <input type="checkbox" id="<?php echo $idPrefix; ?>_filter_format_<?php echo $key; ?>" value="<?php echo $key; ?>" />
+                    </div><label for="<?php echo $idPrefix; ?>_filter_format_<?php echo $key; ?>"><?php echo $type; ?></label>
                 </li>
                 <?php endforeach; ?>
             </ol>
@@ -46,14 +52,14 @@
             <legend><span>ACE Outcomes</span></legend>
             <ol>
                 <li>
-                    <input type="checkbox" id="<?php echo $context->subject; ?>_filterAllACE" class="filterAll" checked="checked" name="allace" value="all" />
-                    <label for="<?php echo $context->subject; ?>_filterAllACE">All ACE</label></li>
+                    <input type="checkbox" id="<?php echo $idPrefix; ?>_filterAllACE" class="filterAll" checked="checked" name="allace" value="all" />
+                    <label for="<?php echo $idPrefix; ?>_filterAllACE">All ACE</label></li>
                 <?php for ($i=1;$i<=10;$i++) : ?>
                 <li>
                     <div class="element">
-                        <input type="checkbox" id="<?php echo $context->subject; ?>_filter_ace_<?php echo $i; ?>" value="ace_<?php echo $i; ?>" />
+                        <input type="checkbox" id="<?php echo $idPrefix; ?>_filter_ace_<?php echo $i; ?>" value="ace_<?php echo $i; ?>" />
                     </div>
-                    <label for="<?php echo $context->subject; ?>_filter_ace_<?php echo $i; ?>"><?php echo $i.' '.UNL_UndergraduateBulletin_ACE::$descriptions[$i]; ?></label>
+                    <label for="<?php echo $idPrefix; ?>_filter_ace_<?php echo $i; ?>"><?php echo $i.' '.UNL_UndergraduateBulletin_ACE::$descriptions[$i]; ?></label>
                 </li>
                 <?php endfor; ?>
             </ol>
