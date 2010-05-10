756183b89944242916bd79bfd006dc4aa00b8acf
Brett Bieber <brett.bieber@gmail.com>
2010-05-10 13:36:26 +0000

    Separate out areas that are not academic colleges.

--- www/templates/html/Introduction.tpl.php
+++ www/templates/html/Introduction.tpl.php
@@ -8,11 +8,14 @@
             <li><a href="college/Architecture">Architecture</a></li>
             <li><a href="college/Arts+%26+Sciences">Arts &amp; Sciences</a></li>
             <li><a href="college/Business+Administration">Business Adminsitration</a></li>
-            <li>Division of General Studies</li>
             <li><a href="college/Education+%26+Human+Sciences">Education &amp; Human Sciences</a></li>
             <li>Engineering</li>
             <li><a href="college/Fine+%26+Performing+Arts">Hixson-Lied College of Fine &amp; Performing Arts</a></li>
             <li>Journalism &amp; Mass Communications</li>
+        </ul>
+        <h3 class="sec_header">Other Areas</h3>
+        <ul id="collegeListing">
+            <li>Division of General Studies</li>
             <li><a href="college/Libraries">Libraries</a></li>
             <li><a href="college/Public+Affairs+%26+Community+Service">Public Affairs &amp; Community Service</a></li>
             <li><a href="college/Reserve+Officers%27+Training+Corps+%28ROTC%29">Reserve Officers' Training Corps (ROTC)</a></li>
