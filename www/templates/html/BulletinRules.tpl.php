a9b4a4b54bc46d21f20d0a008b9b17f11927b9d6
Brett Bieber <brett.bieber@gmail.com>
2010-06-01 14:21:41 +0000

    Add archived bulletins. Update related links.

--- www/templates/html/BulletinRules.tpl.php
+++ www/templates/html/BulletinRules.tpl.php
@@ -1,4 +1,5 @@
 <h2>Bulletin Rules</h2>
+<div class="three_col left">
 <p>Students must recognize that each college has its own bulletin usage rule.
 Students are responsible for knowing which bulletin they should follow.</p>
 <h3>COLLEGE BULLETIN USAGE RULES</h3>
@@ -9,3 +10,21 @@ Students are responsible for knowing which bulletin they should follow.</p>
     }
 }
 ?>
+</div>
+<div class="col right">
+    <div class="zenbox cool">
+        <h3>Previous Bulletins</h3>
+        <ul>
+            <li><a href="downloads/ugb0910.pdf" title="Undergraduate Bulletin 2009-2010, in PDF format (8.6MB)">2009-2010</a></li>
+            <li><a href="downloads/ugb0809.pdf" title="Undergraduate Bulletin 2008-2009, in PDF format (8.7MB)">2008-2009</a></li>
+            <li><a href="downloads/ugb0708.pdf" title="Undergraduate Bulletin 2007-2008, in PDF format (3.2MB)">2007-2008</a></li>
+            <li><a href="downloads/ugb0607.pdf" title="Undergraduate Bulletin 2006-2007, in PDF format (3MB)">2006-2007</a></li>
+            <li><a href="downloads/ugb0506.pdf" title="Undergraduate Bulletin 2005-2006, in PDF format (3MB)">2005-2006</a></li>
+            <li><a href="downloads/ugb0405.pdf" title="Undergraduate Bulletin 2004-2005, in PDF format (3.3MB)">2004-2005</a></li>
+            <li><a href="downloads/ugb0304.pdf" title="Undergraduate Bulletin 2003-2004, in PDF format (2.8MB)">2003-2004</a></li>
+            <li><a href="downloads/ugb0203.pdf" title="Undergraduate Bulletin 2002-2003, in PDF format (3MB)">2002-2003</a></li>
+            <li><a href="downloads/ugb0102.pdf" title="Undergraduate Bulletin 2001-2002, in PDF format (7.8MB)">2001-2002</a></li>
+            <li><a href="downloads/ugb0001.pdf" title="Undergraduate Bulletin 2000-2001, in PDF format (10.6MB)">2000-2001</a></li>
+        </ul>
+    </div>
+</div>
