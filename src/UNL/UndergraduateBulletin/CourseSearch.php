9b55ce8d0078b281d9f1cba8b2d4b083ca9343ca
Brett Bieber <brett.bieber@gmail.com>
2010-06-04 21:31:06 +0000

    Add support for retrieving courses from the local service.

--- src/UNL/UndergraduateBulletin/CourseSearch.php
+++ src/UNL/UndergraduateBulletin/CourseSearch.php
@@ -26,6 +26,12 @@ class UNL_UndergraduateBulletin_CourseSearch implements Countable, UNL_Undergrad
     
     function run()
     {
+        // Try the service
+        if ($results = $this->getServiceResults()) {
+            $this->results = $results;
+            return;
+        }
+
         $search = new UNL_Services_CourseApproval_Search();
 
         $query = $this->options['q'];
@@ -39,6 +45,30 @@ class UNL_UndergraduateBulletin_CourseSearch implements Countable, UNL_Undergrad
         $this->results = $search->byAny($this->options['q'],
                                         $this->options['offset'],
                                         $this->options['limit']);
+    }
+    
+    function getServiceResults()
+    {
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if ($socket === false) {
+            return false;
+        }
+        $result = socket_connect($socket, '127.0.0.1', 13212);
+        if ($result === false) {
+            return false;
+        }
+
+        socket_write($socket, json_encode($this->options)."\n", strlen(json_encode($this->options)."\n"));
+        stream_set_timeout($socket, 2);
+
+        $results = '';
+        while ($line = socket_read($socket, 2048)) {
+            $results .= $line;
+        }
+
+        socket_close($socket);
+
+        return $results;
     }
 
     function count()
