80226ff63c94b40c1c9d0bc436033e865c70dcc3
Brett Bieber <brett.bieber@gmail.com>
2010-06-04 20:10:00 +0000

    Add a course search server which can handle requests. Send json encoded options to this TCP server. Default port is 13212.

---
+++ scripts/courseSearchServer.php
@@ -0,0 +1,89 @@
+#!/usr/local/bin/php -q
+<?php
+require_once dirname(__FILE__).'/../config.sample.php';
+
+error_reporting(E_ALL);
+
+/* Allow the script to hang around waiting for connections. */
+set_time_limit(0);
+
+/* Turn on implicit output flushing so we see what we're getting
+ * as it comes in. */
+ob_implicit_flush();
+
+iconv_set_encoding("internal_encoding", "UTF-8");
+iconv_set_encoding("output_encoding", "UTF-8");
+
+$address = '127.0.0.1';
+$port = 13212;
+
+if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
+    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
+}
+
+if (socket_bind($sock, $address, $port) === false) {
+    echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+}
+
+if (socket_listen($sock, 5) === false) {
+    echo "socket_listen() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+}
+
+$outputcontroller = new Savvy();
+$outputcontroller->setClassToTemplateMapper(new UNL_UndergraduateBulletin_ClassToTemplateMapper());
+$outputcontroller->setEscape('htmlentities');
+
+$search_service = new UNL_Services_CourseApproval_Search();
+
+do {
+    if (($msgsock = socket_accept($sock)) === false) {
+        echo "socket_accept() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+        break;
+    }
+
+    if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
+        echo "socket_read() failed: reason: " . socket_strerror(socket_last_error($msgsock)) . "\n";
+        break 2;
+    }
+    if (!$buf = trim($buf)) {
+        continue;
+    }
+    if ($buf == 'quit') {
+        break;
+    }
+    if ($buf == 'shutdown') {
+        socket_close($msgsock);
+        break 2;
+    }
+
+    $options = json_decode($buf, true);
+
+    if (!is_array($options)) {
+        socket_close($msgsock);
+    }
+
+    $search = new UNL_UndergraduateBulletin_CourseSearch($options);
+
+    $results = $search_service->byAny($search->options['q'],
+                                      $search->options['offset'],
+                                      $search->options['limit']
+                              );
+    $search->results = $results;
+
+    $outputcontroller->setTemplatePath(dirname(dirname(__FILE__)).'/www/templates/html');
+    switch($options['format']) {
+        case 'xml':
+            $outputcontroller->addTemplatePath(dirname(dirname(__FILE__)).'/www/templates/xml');
+            break;
+        case 'json':
+            $outputcontroller->addTemplatePath(dirname(dirname(__FILE__)).'/www/templates/json');
+            break;
+    }
+    $output = $outputcontroller->render($search);
+
+    socket_write($msgsock, $output, strlen($output));
+    echo "$buf\n";
+    socket_close($msgsock);
+} while (true);
+
+socket_close($sock);
