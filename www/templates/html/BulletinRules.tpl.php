03c97cd93f04d0886a23a6e6376ab7d9ff058dd7
Seth Meranda <smeranda2@unl.edu>
2010-09-15 18:29:10 +0000

    change & to &amp; for validation in college name.

--- www/templates/html/BulletinRules.tpl.php
+++ www/templates/html/BulletinRules.tpl.php
@@ -5,7 +5,7 @@ Students are responsible for knowing which bulletin they should follow.</p>
 <h3>COLLEGE BULLETIN USAGE RULES</h3>
 <?php foreach (new UNL_UndergraduateBulletin_CollegeList() as $college) {
     if (isset($college->description->bulletinRule)) {
-        echo '<h4 class="sec_main">'.$college->name.'</h4>';
+        echo '<h4 class="sec_main">'.htmlspecialchars($college->name).'</h4>';
         echo $college->description->bulletinRule;
     }
 }
