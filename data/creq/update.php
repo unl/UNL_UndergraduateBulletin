ccf6ada9d92e9cd7de7a0595a2ab49c4555e0293
Brett Bieber <brett.bieber@gmail.com>
2010-11-08 15:19:29 +0000

    Add notes.

--- data/creq/update.php
+++ data/creq/update.php
@@ -1,5 +1,5 @@
 <?php
-
+echo 'Now retrieving all courses by subject code.'.PHP_EOL;
 $handle = fopen(dirname(__FILE__).'/subject_codes.csv', 'r');
 while (($subject = fgetcsv($handle, 1000, ",", "'")) !== false) {
     if ($data = file_get_contents('http://creq.unl.edu/courses/public-view/all-courses/subject/'.$subject[0])) {
@@ -8,5 +8,12 @@ while (($subject = fgetcsv($handle, 1000, ",", "'")) !== false) {
         echo 'Could not retrieve data for '.$subject[0].PHP_EOL;
     }
 }
+echo 'Done'.PHP_EOL;
 
+echo 'Creating minimized course data file for JSON output.'.PHP_EOL;
 include dirname(__FILE__).'/minimize.php';
+echo 'Done'.PHP_EOL;
+
+echo 'Updating course search database for speedy searches.'.PHP_EOL;
+include dirname(__FILE__).'/../../scripts/build_course_db.php';
+echo 'Done'.PHP_EOL;
