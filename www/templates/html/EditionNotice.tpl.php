1e4e67f3a57b845a2152c066b93d5c2d49211c95
Brett Bieber <brett.bieber@gmail.com>
2011-02-14 21:05:42 +0000

    Add a link to the latest bulletin within the edition notice, and slight margin fix.

--- www/templates/html/EditionNotice.tpl.php
+++ www/templates/html/EditionNotice.tpl.php
@@ -1,12 +1,17 @@
+<?php
+$latest  = UNL_UndergraduateBulletin_Editions::getLatest();
+$current = UNL_UndergraduateBulletin_Controller::getEdition();
+?>
 <div id="versioning">
     <div class="content">
-        <h5>This content is the <span><?php echo UNL_UndergraduateBulletin_Controller::getEdition()->getRange(); ?></span> Undergraduate Bulletin</h5>
+        <h5>This content is the <span><?php echo $current->getRange(); ?></span> Undergraduate Bulletin</h5>
         <p>Other versions:</p>
         <ul>
+            <li><a href="<?php echo $latest->getURL(); ?>"><?php echo $latest->getRange(); ?> <em>(includes latest course descriptions)</em></a></li>
             <?php foreach (UNL_UndergraduateBulletin_Editions::getPublished() as $edition): ?>
-            <li><a href="<?php echo $edition->getURL(); ?>"><?php echo $edition->getRange(); ?> Undergraduate Bulletin</a></li>
+            <li><a href="<?php echo $edition->getURL(); ?>"><?php echo $edition->getRange(); ?> Archive Edition</a></li>
             <?php endforeach; ?>
-            <li><a href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>bulletinrules">More</a></li>
+            <li><a href="<?php echo $current->getURL(); ?>bulletinrules">More info, and other archived bulletins</a></li>
         </ul>
     </div>
     <div class="action opened">
