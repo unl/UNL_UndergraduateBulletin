d66b7dcc0d30e1cf1ab9c3cec4d995a66fd493d6
Brett Bieber <brett.bieber@gmail.com>
2011-02-14 22:09:53 +0000

    Visually indicate which version you're currently viewing.

--- www/templates/html/EditionNotice.tpl.php
+++ www/templates/html/EditionNotice.tpl.php
@@ -7,9 +7,20 @@ $current = UNL_UndergraduateBulletin_Controller::getEdition();
         <h5>This content is the <span><?php echo $current->getRange(); ?></span> Undergraduate Bulletin</h5>
         <p>Other versions:</p>
         <ul>
-            <li><a href="<?php echo $latest->getURL(); ?>"><?php echo $latest->getRange(); ?> <em>(includes latest course descriptions)</em></a></li>
-            <?php foreach (UNL_UndergraduateBulletin_Editions::getPublished() as $edition): ?>
-            <li><a href="<?php echo $edition->getURL(); ?>"><?php echo $edition->getRange(); ?> Archive Edition</a></li>
+            <?php
+            $class = '';
+            if ($latest->getURL() == $current->getURL()) {
+                $class = 'selected';
+            }
+            ?>
+            <li class="<?php echo $class; ?>"><a href="<?php echo $latest->getURL(); ?>"><?php echo $latest->getRange(); ?> <em>(includes latest course descriptions)</em></a></li>
+            <?php foreach (UNL_UndergraduateBulletin_Editions::getPublished() as $edition):
+            $class = '';
+            if ($edition->getURL() == $current->getURL()) {
+                $class = 'selected';
+            }
+            ?>
+            <li class="<?php echo $class; ?>"><a href="<?php echo $edition->getURL(); ?>"><?php echo $edition->getRange(); ?> Archive Edition</a></li>
             <?php endforeach; ?>
             <li><a href="<?php echo $current->getURL(); ?>bulletinrules">More info, and other archived bulletins</a></li>
         </ul>
