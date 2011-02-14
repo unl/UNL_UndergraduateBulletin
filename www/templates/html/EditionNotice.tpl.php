2690d92fd7e50dbadef06c52cba7a53f66e7a331
Brett Bieber <brett.bieber@gmail.com>
2011-02-14 17:35:52 +0000

    Only display published editions when listing all of the versions

--- www/templates/html/EditionNotice.tpl.php
+++ www/templates/html/EditionNotice.tpl.php
@@ -3,7 +3,7 @@
         <h5>This content is the <span><?php echo UNL_UndergraduateBulletin_Controller::getEdition()->getRange(); ?></span> Undergraduate Bulletin</h5>
         <p>Other versions:</p>
         <ul>
-            <?php foreach (UNL_UndergraduateBulletin_Editions::getAll() as $edition): ?>
+            <?php foreach (UNL_UndergraduateBulletin_Editions::getPublished() as $edition): ?>
             <li><a href="<?php echo $edition->getURL(); ?>"><?php echo $edition->getRange(); ?> Undergraduate Bulletin</a></li>
             <?php endforeach; ?>
             <li><a href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>bulletinrules">More</a></li>
