c7226f53da54d0c192331fc3a627164a5c654ae6
Brett Bieber <brett.bieber@gmail.com>
2011-02-23 21:47:42 +0000

    Add a controller for the editor views, and a majorsummaryform

---
+++ src/UNL/UndergraduateBulletin/Editor/Controller.php
@@ -0,0 +1,49 @@
+<?php
+class UNL_UndergraduateBulletin_Editor_Controller extends UNL_UndergraduateBulletin_LoginRequired implements UNL_UndergraduateBulletin_CacheableInterface, UNL_UndergraduateBulletin_PostRunReplacements
+{
+    public $output;
+
+    public $options = array('view'=>'summaryform');
+
+    protected $view_map = array(
+        'summaryform'     => 'UNL_UndergraduateBulletin_Editor_MajorSummaryForm',
+        );
+
+    public function __postConstruct()
+    {
+        
+    }
+
+    function preRun()
+    {
+        
+    }
+
+    function run()
+    {
+        try {
+            if (isset($this->view_map[$this->options['view']])) {
+                $this->output[] = new $this->view_map[$this->options['view']]($this->options);
+            } else {
+                $this->output[] = new Exception('Sorry, that view does not exist.');
+            }
+        } catch(Exception $e) {
+            $this->output[] = $e;
+        }
+    }
+
+    function getCacheKey()
+    {
+        return false;
+    }
+
+    public static function setReplacementData($field, $value)
+    {
+        
+    }
+
+    function postRun($data)
+    {
+        return $data;
+    }
+}
---
+++ src/UNL/UndergraduateBulletin/Editor/MajorSummaryForm.php
@@ -0,0 +1,8 @@
+<?php
+class UNL_UndergraduateBulletin_Editor_MajorSummaryForm extends UNL_UndergraduateBulletin_LoginRequired
+{
+    function __postConstruct()
+    {
+        
+    }
+}
