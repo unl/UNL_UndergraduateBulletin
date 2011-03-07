418a249ba6a7eae51458e1934d696b55bf929d8f
Brett Bieber <brett.bieber@gmail.com>
2011-03-07 15:56:30 +0000

    Improve error messages when subject code mappings fail.

--- src/UNL/UndergraduateBulletin/Major/SubjectAreas.php
+++ src/UNL/UndergraduateBulletin/Major/SubjectAreas.php
@@ -6,12 +6,23 @@ class UNL_UndergraduateBulletin_Major_SubjectAreas extends ArrayIterator
     function __construct($major)
     {
         $subject_codes = array();
+
         $this->major = $major;
+
         $mapping = file_get_contents(UNL_UndergraduateBulletin_Controller::getEdition()->getDataDir().'/major_to_subject_code.php.ser');
-        $mapping = unserialize($mapping);
+
+        if (false === ($mapping = unserialize($mapping))) {
+            throw new Exception('Invalid major to subject code matching file.', 500);
+        }
+
         if (isset($mapping[$this->major->title])) {
             $subject_codes = $mapping[$this->major->title];
+
+            if (!is_array($subject_codes)) {
+                throw new Exception('Subject codes for '.$major.' are formatted incorrectly in the map file.', 500);
+            }
         }
+
         parent::__construct($subject_codes);
     }
     
