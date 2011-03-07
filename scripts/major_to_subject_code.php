310f42631125b8347a8d20c0f382ae8d19f0f2ef
Brett Bieber <brett.bieber@gmail.com>
2011-03-07 15:55:40 +0000

    Go through every edition and update subject code mappings.

--- scripts/major_to_subject_code.php
+++ scripts/major_to_subject_code.php
@@ -2,26 +2,41 @@
 
 require dirname(__FILE__).'/../config.sample.php';
 
-$file = file(UNL_UndergraduateBulletin_Controller::getEdition()->getDataDir().'/major_to_subject_code.csv');
+foreach (new UNL_UndergraduateBulletin_Editions() as $edition) {
 
-$majors = array();
+    UNL_UndergraduateBulletin_Controller::setEdition($edition);
 
-foreach ($file as $line) {
-    $array = str_getcsv($line);
+    $file = file($edition->getDataDir().'/major_to_subject_code.csv');
+
+    $majors = array();
+
+    foreach ($file as $line) {
+        $array = str_getcsv($line);
+        /*
+        For example:
+        
+        array(3) {
+          [0]=>
+          string(13) "Water Science"
+          [1]=>
+          string(3) "ANR"
+          [2]=>
+          string(4) "WATS"
+        }
+        
+        */
     
-    $major = new UNL_UndergraduateBulletin_Major(array('title'=>$array[0]));
-    
-//    0 = major
-//    1 = college
-//    2 = codes
-    $codes = array();
-    if (isset($array[2])
-        && trim($array[2]) != 'NO COURSE TAB') {
-        $codes = explode(',', str_replace(' ', '', $array[2]));
+        $major = new UNL_UndergraduateBulletin_Major(array('title'=>$array[0]));
+
+        $codes = array();
+        if (isset($array[2])
+            && trim($array[2]) != 'NO COURSE TAB') {
+            $codes = explode(',', str_replace(' ', '', $array[2]));
+        }
+        $codes = array_unique($codes);
+        sort($codes);
+        $majors[trim($array[0])] = $codes;
     }
-    $codes = array_unique($codes);
-    sort($codes);
-    $majors[trim($array[0])] = $codes;
-}
 
-file_put_contents(UNL_UndergraduateBulletin_Controller::getEdition()->getDataDir().'/major_to_subject_code.php.ser', serialize($majors));
+    file_put_contents($edition->getDataDir().'/major_to_subject_code.php.ser', serialize($majors));
+}
