d42cafa19739766842b18bc76fc8ee6078779600
Brett Bieber <brett.bieber@gmail.com>
2011-05-20 21:53:31 +0000

    Move Major College requirements into a separate template.

---
+++ www/templates/html/Major/Colleges.tpl.php
@@ -0,0 +1,22 @@
+<?php
+$college_regions = array(
+    'admissionRequirements'         => 'College Admission',
+    'degreeRequirements'            => 'College Degree Requirements',
+    'aceRequirements'               => 'ACE Requirements',
+    'bulletinRule'                  => 'Bulletin Rule',
+    );
+foreach ($context as $college) {
+    foreach ($college_regions as $id=>$title) {
+        if (isset($college->description->$id)) { 
+            $college_section = $college->description->getRaw($id);
+            ?>
+            <div id="college_<?php echo $id; ?>">
+                <h3 class="sec_header"><?php echo $title; ?></h3>
+                <?php echo str_replace(array('<h3', '</h3>'),
+                                       array('<h4', '</h4>'), $college_section); ?>
+            </div>
+            <?php
+        }
+    }
+}
+?>
--- www/templates/html/Major/Description.tpl.php
+++ www/templates/html/Major/Description.tpl.php
@@ -9,12 +9,6 @@
     'ace_requirements'              => 'Ace Requirements',
     'other'                         => 'Other',
     );
-    $college_regions = array(
-    'admissionRequirements'         => 'College Admission',
-    'degreeRequirements'            => 'College Degree Requirements',
-    'aceRequirements'               => 'ACE Requirements',
-    'bulletinRule'                  => 'Bulletin Rule',
-    );
 ?>
 <div class="three_col left">
     <div id="toc_nav">
@@ -32,22 +26,7 @@
         }
         ?>
         <h2 id="college_requirements" class="sec_header">COLLEGE REQUIREMENTS</h2>
-        <?php
-        foreach ($context->colleges as $college) {
-            foreach ($college_regions as $id=>$title) {
-                if (isset($college->description->$id)) { 
-                    $college_section = $college->description->getRaw($id);
-                    ?>
-                    <div id="college_<?php echo $id; ?>">
-                        <h3 class="sec_header"><?php echo $title; ?></h3>
-                        <?php echo str_replace(array('<h3', '</h3>'),
-                                               array('<h4', '</h4>'), $college_section); ?>
-                    </div>
-                    <?php
-                }
-            }
-        }
-        ?>
+        <?php echo $savvy->render($context->colleges); ?>
     </div>
 </div>
 <div class="col right">
