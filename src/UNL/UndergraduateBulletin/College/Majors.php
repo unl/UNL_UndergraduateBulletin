eb06d8ab6319b047121e98debabe4fa1441661b7
Brett Bieber <brett.bieber@gmail.com>
2011-06-18 04:39:11 +0000

    Allow the major name to be passed in options.

--- src/UNL/UndergraduateBulletin/College/Majors.php
+++ src/UNL/UndergraduateBulletin/College/Majors.php
@@ -1,10 +1,18 @@
 <?php
 class UNL_UndergraduateBulletin_College_Majors extends FilterIterator
 {
+	/**
+	 * The college
+	 *
+	 * @var UNL_UndergraduateBulletin_College
+	 */
     protected $college;
 
     function __construct($options = array())
     {
+    	if (!isset($options['college'])) {
+    		$options['college'] = new UNL_UndergraduateBulletin_College($options);
+    	}
         $this->college = $options['college'];
         parent::__construct(new UNL_UndergraduateBulletin_MajorList);
     }
