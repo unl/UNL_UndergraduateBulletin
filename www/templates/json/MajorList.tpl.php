b8284dd157e9e69b0af2259ad83bcc610f508149
Brett Bieber <brett.bieber@gmail.com>
2011-06-20 14:01:12 +0000

    Add URI to the fields available for majors

--- www/templates/json/MajorList.tpl.php
+++ www/templates/json/MajorList.tpl.php
@@ -5,7 +5,8 @@ foreach ($context as $major) {
     $majors[] = array(
         'title'          => $major->getRaw('title'),
         'minorAvailable' => $major->minorAvailable(),
-        'college'        => (isset($major->colleges)?$major->colleges->getArray():'')
+        'college'        => (isset($major->colleges)?$major->colleges->getArray():''),
+        'uri'			 => $major->getURL()
         );
 }
 echo json_encode($majors);
