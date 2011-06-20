3a91c24063ee58c0cf7687955ef68bee1b4e2130
Brett Bieber <brett.bieber@gmail.com>
2011-06-20 13:45:34 +0000

    Add proper heading, titles, and breadcrumbs

--- www/templates/html/College/Majors.tpl.php
+++ www/templates/html/College/Majors.tpl.php
@@ -1,5 +1,15 @@
-<h2 class="college-name"><?php echo $context->college->name; ?></h2>
 <?php
+$url = UNL_UndergraduateBulletin_Controller::getURL();
+UNL_UndergraduateBulletin_Controller::setReplacementData('doctitle', 'UNL | Undergraduate Bulletin | '.$context->college->name.' | Majors');
+UNL_UndergraduateBulletin_Controller::setReplacementData('pagetitle', '<h2 class="college-name">'.$context->college->name.'</h2>');
+UNL_UndergraduateBulletin_Controller::setReplacementData('breadcrumbs', '
+<ul>
+    <li><a href="http://www.unl.edu/">UNL</a></li>
+    <li><a href="'.$url.'">Undergraduate Bulletin</a></li>
+    <li><a href="'.$context->college->getURL().'">'.$context->college->name.'</a></li>
+    <li>Majors</li>
+</ul>
+');
 if (count($context)) {
     echo $savvy->render($context, 'MajorList/UnorderedList.tpl.php');
 }
