75bb85e6540f43e3459734d9b1659c152571a13e
Brett Bieber <brett.bieber@gmail.com>
2011-06-21 21:44:22 +0000

    Add template for a list of colleges

---
+++ www/templates/json/CollegeList.tpl.php
@@ -0,0 +1,11 @@
+<?php
+$colleges = array();
+
+foreach ($context as $abbreviation=>$college) {
+    $colleges[] = array(
+        'abbreviation'  => $abbreviation,
+        'name'          => $college->getRaw('name'),
+        'uri'			=> $college->getURL()
+        );
+}
+echo json_encode($colleges);
