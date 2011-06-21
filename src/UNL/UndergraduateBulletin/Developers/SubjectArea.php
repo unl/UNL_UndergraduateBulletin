ff2e8bcb30aec9d2c9b5621d5c9e238c593f56a1
Brett Bieber <brett.bieber@gmail.com>
2011-06-21 21:52:12 +0000

    Add developer documentation for accessing the SubjectArea resource

---
+++ src/UNL/UndergraduateBulletin/Developers/SubjectArea.php
@@ -0,0 +1,23 @@
+<?php
+class UNL_UndergraduateBulletin_Developers_SubjectArea
+{
+    public $title       = "Subject Area";
+    
+    public $uri         = "courses/{subjectArea}";
+    
+    public $exampleURI  = "courses/ECON";
+    
+    public $properties  = array(
+                                array("id", "(String) The subject code", true, true),
+                                array("title", "(String) The title of the subject area", true, true),
+                                array("courses", "(Array) The courses", true, true),
+                                );
+                                
+    public $formats     = array("json", "xml", "partial");
+    
+    function __construct()
+    {
+        $this->uri = "http://" . $_SERVER['SERVER_NAME'].UNL_UndergraduateBulletin_Controller::$url . $this->uri;
+        $this->exampleURI  = "http://" . $_SERVER['SERVER_NAME'].UNL_UndergraduateBulletin_Controller::$url . $this->exampleURI;
+    }
+}
