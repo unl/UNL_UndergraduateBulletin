535adccb67f4dd5201f66885badf0741d4511e1b
Brett Bieber <brett.bieber@gmail.com>
2011-06-21 21:04:17 +0000

    Add newline between xml preamble and root element

--- www/templates/xml/Controller.tpl.php
+++ www/templates/xml/Controller.tpl.php
@@ -1,4 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL; ?>
 <courses xmlns="http://courseapproval.unl.edu/courses" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://courseapproval.unl.edu/courses /schema/courses.xsd">
 <?php
 echo $savvy->render($context->output);
