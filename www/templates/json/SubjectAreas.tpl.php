e13e2f0c17914676ab2bafc997784be141efcb7f
Kevin Abel <kabel2@unl.edu>
2011-07-05 17:42:09 +0000

    Added a JSON template for the subject area list.

---
+++ www/templates/json/SubjectAreas.tpl.php
@@ -0,0 +1,10 @@
+<?php
+$subjects = array();
+foreach ($context as $subject_code=>$area) {
+    $subjects[$subject_code] = array(
+        'href' => 'http://'.$_SERVER['SERVER_NAME'].UNL_UndergraduateBulletin_Controller::getURL().'courses/'.$subject_code.'/',
+        'title' => $area->getRaw('title')
+    );
+}
+
+echo json_encode($subjects);
