3bbcf93d8b51402afa6123a99c81a22f6c25874e
Kevin Abel <kabel2@unl.edu>
2011-07-05 17:35:12 +0000

    Moved the COURSES xml root element into the respective template files to allow the Controller template to render other object to XML.

--- www/templates/xml/Course.tpl.php
+++ www/templates/xml/Course.tpl.php
@@ -1,2 +1,5 @@
+<courses xmlns="http://courseapproval.unl.edu/courses" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://courseapproval.unl.edu/courses /schema/courses.xsd">
 <?php
 echo str_replace('<?xml version="1.0"?>', '', $context->getRawObject()->asXML());
+?>
+</courses>
--- www/templates/xml/SubjectArea.tpl.php
+++ www/templates/xml/SubjectArea.tpl.php
@@ -1,5 +1,7 @@
+<courses xmlns="http://courseapproval.unl.edu/courses" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://courseapproval.unl.edu/courses /schema/courses.xsd">
 <?php
 foreach ($context->courses as $course) {
     echo $savvy->render($course);
 }
 ?>
+</courses>
