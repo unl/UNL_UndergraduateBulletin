ff87448750498f7aa1569ef6e4e28bfb1bcaeb53
Kevin Abel <kabel2@unl.edu>
2011-07-05 17:46:11 +0000

    Added the data dictionary from PeopleSoft for the major codes (slightly different than subject codes). Data scraped from http://www.unl.edu/regrec/epm/dd/values/fv_acad_plan.shtml

---
+++ scripts/acad_plan_to_major_lookup.php
@@ -0,0 +1,35 @@
+<?php
+
+require dirname(__FILE__).'/../config.sample.php';
+
+foreach (new UNL_UndergraduateBulletin_Editions() as $edition) {
+
+    UNL_UndergraduateBulletin_Controller::setEdition($edition);
+
+    $file = file($edition->getDataDir().'/acad_plan_full.csv');
+
+    $majors = array();
+
+    foreach ($file as $line) {
+        $array = str_getcsv($line);
+        /*
+        For example:
+
+        array(2) {
+          [0]=>
+          string(9) "AACTS-MAJ"
+          [1]=>
+          string(17) "Actuarial Science"
+        }
+
+        */
+
+        $plan = explode('-', $array[0]);
+
+        if (!array_key_exists($plan[0], $majors)) {
+            $majors[$plan[0]] = $array[1];
+        }
+    }
+
+    file_put_contents($edition->getDataDir().'/major_lookup.php.ser', serialize($majors));
+}
