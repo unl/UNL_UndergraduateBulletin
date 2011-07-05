30608badad14564aeeff2c691e27d1ee1c05410d
Kevin Abel <kabel2@unl.edu>
2011-07-05 17:36:17 +0000

    Added a subject areas XML template that mimics that formatting of the graduate bulletin.

---
+++ www/templates/xml/SubjectAreas.tpl.php
@@ -0,0 +1,5 @@
+<subjects xmlns:xlink="http://www.w3.org/1999/xlink">
+<?php foreach ($context as $subject_code=>$area): ?>
+    <s xlink:href="<?php echo 'http://'.$_SERVER['SERVER_NAME'].UNL_UndergraduateBulletin_Controller::getURL().'courses/'.$subject_code.'/' ?>" xml:id="<?php echo $subject_code ?>"><?php echo $area->title ?></s>
+<?php endforeach; ?>
+</subjects>
