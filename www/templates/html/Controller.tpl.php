734eb551eac80bfb2c17dcb66b728bd0460a0467
Brett Bieber <brett.bieber@gmail.com>
2011-07-06 19:33:39 +0000

    Move navigation into a separate template for easier maintenance.

--- www/templates/html/Controller.tpl.php
+++ www/templates/html/Controller.tpl.php
@@ -15,57 +15,8 @@ $page->breadcrumbs  = '
 </ul>
 ';
 
-$page->navlinks     = '
-<ul>
-    <li><a href="'.$url.'major/">Majors</a>
-        <ul>
-            <li><a href="'.$url.'major/search">Search for a Major</a></li>
-        </ul>
-    </li>
-    <li><a href="'.$url.'courses/">Courses</a>
-        <ul>
-            <li><a href="'.$url.'courses/search">Search for a Course</a></li>
-            <li><a href="'.$url.'courses/">Course Abbreviations</a></li>
-        </ul>
-    </li>
-    <li><a href="'.$url.'general">Academic Policies</a>
-        <ul>
-            <li><a href="'.$url.'general#admission-categories">Admission Categories</a></li>
-            <li><a href="'.$url.'general#undergraduate-transfer-credit-policy">Transfer Credit Policy</a></li>
-            <li><a href="'.$url.'general#graduation-requirements">Graduation Requirements</a></li>
-            <li><a href="'.$url.'general#academic-policies-and-procedures">Academic Policies and Procedures</a></li>
-        </ul>
-    </li>
-    <li><a href="'.$url.'college/">Academic Colleges</a>
-        <ul>
-            <li><a href="'.$url.'college/Agricultural+Sciences+%26+Natural+Resources">Agricultural Sciences &amp; Natural Resources</a></li>
-            <li><a href="'.$url.'college/Architecture">Architecture</a></li>
-            <li><a href="'.$url.'college/Arts+%26+Sciences">Arts &amp; Sciences</a></li>
-            <li><a href="'.$url.'college/Business+Administration">Business Administration</a></li>
-            <li><a href="'.$url.'college/Education+%26+Human+Sciences">Education &amp; Human Sciences</a></li>
-            <li><a href="'.$url.'college/Engineering">Engineering</a></li>
-            <li><a href="'.$url.'college/Fine+%26+Performing+Arts">Hixson-Lied College of Fine &amp; Performing Arts</a></li>
-            <li><a href="'.$url.'college/Journalism+%26+Mass+Communications">Journalism &amp; Mass Communications</a></li>
-            
-        </ul>
-    </li>
-    <li><a href="#">Honors Programs</a>
-        <ul>
-            <li><a href="'.$url.'college/Office+of+Undergraduate+Studies#university-honors-program-">NU Honors Program</a></li>
-            <li><a href="#">Jeffrey S. Raikes School of Computer Science and Management</a></li>
-        </ul>
-    </li>
-    <li><a href="'.$url.'college/">Other Academic Units</a>
-        <ul>
-            <li><a href="'.$url.'college/Office+of+Undergraduate+Studies">Office of Undergraduate Studies</a></li>
-            <li><a href="'.$url.'college/Division+of+General+Studies">Division of General Studies</a></li>
-            <li><a href="'.$url.'college/Libraries">Libraries</a></li>
-            <li><a href="'.$url.'college/Public+Affairs+%26+Community+Service">Public Affairs &amp; Community Service</a></li>
-            <li><a href="'.$url.'college/Reserve+Officers+Training+Corps+%28ROTC%29">Reserve Officers Training Corps (ROTC)</a></li>
-        </ul>
-    </li>
-    
-</ul>';
+$page->navlinks = $savvy->render(null, 'Navigation.tpl.php');
+
 $page->loadSharedCodeFiles();
 $page->addStylesheet('/wdn/templates_3.0/css/content/notice.css');
 $page->addStylesheet('/wdn/templates_3.0/css/content/zenform.css');
---
+++ www/templates/html/Navigation.tpl.php
@@ -0,0 +1,51 @@
+<?php
+$url = UNL_UndergraduateBulletin_Controller::getURL();
+?>
+<ul>
+    <li><a href="<?php echo $url; ?>major/">Majors</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>major/search">Search for a Major</a></li>
+        </ul>
+    </li>
+    <li><a href="<?php echo $url; ?>courses/">Courses</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>courses/search">Search for a Course</a></li>
+            <li><a href="<?php echo $url; ?>courses/">Course Abbreviations</a></li>
+        </ul>
+    </li>
+    <li><a href="<?php echo $url; ?>general">Academic Policies</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>general#admission-categories">Admission Categories</a></li>
+            <li><a href="<?php echo $url; ?>general#undergraduate-transfer-credit-policy">Transfer Credit Policy</a></li>
+            <li><a href="<?php echo $url; ?>general#graduation-requirements">Graduation Requirements</a></li>
+            <li><a href="<?php echo $url; ?>general#academic-policies-and-procedures">Academic Policies and Procedures</a></li>
+        </ul>
+    </li>
+    <li><a href="<?php echo $url; ?>college/">Academic Colleges</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>college/Agricultural+Sciences+%26+Natural+Resources">Agricultural Sciences &amp; Natural Resources</a></li>
+            <li><a href="<?php echo $url; ?>college/Architecture">Architecture</a></li>
+            <li><a href="<?php echo $url; ?>college/Arts+%26+Sciences">Arts &amp; Sciences</a></li>
+            <li><a href="<?php echo $url; ?>college/Business+Administration">Business Administration</a></li>
+            <li><a href="<?php echo $url; ?>college/Education+%26+Human+Sciences">Education &amp; Human Sciences</a></li>
+            <li><a href="<?php echo $url; ?>college/Engineering">Engineering</a></li>
+            <li><a href="<?php echo $url; ?>college/Fine+%26+Performing+Arts">Hixson-Lied College of Fine &amp; Performing Arts</a></li>
+            <li><a href="<?php echo $url; ?>college/Journalism+%26+Mass+Communications">Journalism &amp; Mass Communications</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Honors Programs</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>college/Office+of+Undergraduate+Studies#university-honors-program-">NU Honors Program</a></li>
+            <li><a href="#">Jeffrey S. Raikes School of Computer Science and Management</a></li>
+        </ul>
+    </li>
+    <li><a href="<?php echo $url; ?>college/">Other Academic Units</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>college/Office+of+Undergraduate+Studies">Office of Undergraduate Studies</a></li>
+            <li><a href="<?php echo $url; ?>college/Division+of+General+Studies">Division of General Studies</a></li>
+            <li><a href="<?php echo $url; ?>college/Libraries">Libraries</a></li>
+            <li><a href="<?php echo $url; ?>college/Public+Affairs+%26+Community+Service">Public Affairs &amp; Community Service</a></li>
+            <li><a href="<?php echo $url; ?>college/Reserve+Officers+Training+Corps+%28ROTC%29">Reserve Officers Training Corps (ROTC)</a></li>
+        </ul>
+    </li>
+</ul>
