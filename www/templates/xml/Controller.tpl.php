723289f8f35e131d8763ba7527dd179ba2310584
Brett Bieber <brett.bieber@gmail.com>
2011-08-18 19:36:51 +0000

    Move courses root element logic up to reduce number of checks when large sets of courses are returned.

--- www/templates/xml/Controller.tpl.php
+++ www/templates/xml/Controller.tpl.php
@@ -1,4 +1,12 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL;
 
+if ($context->options['view'] == 'course') {
+    echo '<courses xmlns="http://courseapproval.unl.edu/courses" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://courseapproval.unl.edu/courses /schema/courses.xsd">'.PHP_EOL;
+}
+
 echo $savvy->render($context->output);
+
+if ($context->options['view'] == 'course') {
+    echo '</courses>';
+}
 ?>
--- www/templates/xml/Course.tpl.php
+++ www/templates/xml/Course.tpl.php
@@ -1,11 +1,3 @@
 <?php
-if ($parent->parent->context->options['view'] == 'course') {
-	echo '<courses xmlns="http://courseapproval.unl.edu/courses" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://courseapproval.unl.edu/courses /schema/courses.xsd">'.PHP_EOL;
-}
-
 echo '  '.str_replace('<?xml version="1.0"?>', '', $context->getRawObject()->asXML()).PHP_EOL;
-
-if ($parent->parent->context->options['view'] == 'course') {
-	echo '</courses>';
-}
 ?>
