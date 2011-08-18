5a06c6a8334e0b56033dbe070b94f8154f381fbd
Brett Bieber <brett.bieber@gmail.com>
2011-08-18 19:35:16 +0000

    Only show root courses element if this is a course view.

--- www/templates/xml/Course.tpl.php
+++ www/templates/xml/Course.tpl.php
@@ -1,5 +1,11 @@
-<courses xmlns="http://courseapproval.unl.edu/courses" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://courseapproval.unl.edu/courses /schema/courses.xsd">
 <?php
-echo str_replace('<?xml version="1.0"?>', '', $context->getRawObject()->asXML());
+if ($parent->parent->context->options['view'] == 'course') {
+	echo '<courses xmlns="http://courseapproval.unl.edu/courses" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://courseapproval.unl.edu/courses /schema/courses.xsd">'.PHP_EOL;
+}
+
+echo '  '.str_replace('<?xml version="1.0"?>', '', $context->getRawObject()->asXML()).PHP_EOL;
+
+if ($parent->parent->context->options['view'] == 'course') {
+	echo '</courses>';
+}
 ?>
-</courses>
