960ead979a846c72a9c5d6f56308c54cbb64852e
Brett Bieber <brett.bieber@gmail.com>
2011-08-30 21:52:35 +0000

    Use the NULL caching service so the template is pulled from local tpl_cache

--- www/templates/html/Controller.tpl.php
+++ www/templates/html/Controller.tpl.php
@@ -1,4 +1,5 @@
 <?php
+UNL_Templates::setCachingService(new UNL_Templates_CachingService_Null());
 UNL_Templates::$options['version']        = 3;
 UNL_Templates::$options['sharedcodepath'] = dirname(__FILE__).'/sharedcode';
 
