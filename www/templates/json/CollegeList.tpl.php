2005dbeed724fa7b1eb26d651d08a229b5735d8f
Brett Bieber <brett.bieber@gmail.com>
2011-12-15 08:43:16 -0600

    Whitespace, tabs to spaces

--- www/templates/json/CollegeList.tpl.php
+++ www/templates/json/CollegeList.tpl.php
@@ -5,7 +5,7 @@ foreach ($context as $abbreviation=>$college) {
     $colleges[] = array(
         'abbreviation'  => $abbreviation,
         'name'          => $college->getRaw('name'),
-        'uri'			=> $college->getURL()
+        'uri'           => $college->getURL()
         );
 }
 echo json_encode($colleges);
