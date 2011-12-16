2c788d36ea80889056125ac8ac71815b0fca8b5c
Seth Meranda <seth@meranda.org>
2011-12-16 11:33:52 -0600

    Remove zenform, make markup a bit more lean.

--- www/templates/html/CourseSearchForm.tpl.php
+++ www/templates/html/CourseSearchForm.tpl.php
@@ -1,15 +1,11 @@
-<form class="zenform cool coursesearch" action="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search" method="get">
+<form class="coursesearch" action="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search" method="get">
     <fieldset>
         <legend>Find a Course</legend>
-    <ol>
-    <li>
         <label for="courseSearch"><span class="required">*</span>Course</label>
         <input type="text" name="q" id="courseSearch" value="<?php echo (isset($parent->context->options['q']))?htmlentities($parent->context->options['q'], ENT_QUOTES):''; ?>" />
-    </li>
-    </ol>
+        <input type="submit" value="Find" name="submit" />
     </fieldset>
-    <input type="submit" value="Search" name="submit" />
-    <div id="courseSearchHelp" style="display:none">
+    <div id="courseSearchHelp">
         <h4>Searching for a Course</h4>
         <p>Search by Course Title (<span class="operator">Global Advertising</span>), Course Code (<span class="operator">ADVT</span>), Course Number (<span class="operator">438</span>) or advanced:</p>
         <ul>
--- www/templates/html/MajorList/SearchForm.tpl.php
+++ www/templates/html/MajorList/SearchForm.tpl.php
@@ -1,13 +1,8 @@
-<form class="zenform cool" action="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>major/search" method="get" style="margin-top:10px;">
+<form class="" action="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>major/search" method="get" style="margin-top:10px;">
     <fieldset>
         <legend>Find a Major or Degree</legend>
-    <ol>
-    <li>
         <label for="majorSearch"><span class="required">*</span>Major or Degree</label>
         <input type="text" name="q" id="majorSearch"  value="<?php echo (isset($parent->context->options['q']))?htmlentities($parent->context->options['q'], ENT_QUOTES):''; ?>" />
-        
-    </li>
-    </ol>
+        <input type="submit" value="Find" name="submit" />
     </fieldset>
-    <input type="submit" value="Search" name="submit" />
 </form>
