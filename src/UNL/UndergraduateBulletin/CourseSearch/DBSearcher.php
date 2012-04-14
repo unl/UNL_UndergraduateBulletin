8c95e68b1e62877f708e74ea3335c0075a859b33
Brett Bieber <brett.bieber@gmail.com>
2012-04-14 15:01:22 -0500

    Modify global WHERE clause to exclude graduate courses

--- src/UNL/UndergraduateBulletin/CourseSearch/DBSearcher.php
+++ src/UNL/UndergraduateBulletin/CourseSearch/DBSearcher.php
@@ -101,7 +101,7 @@ class UNL_UndergraduateBulletin_CourseSearch_DBSearcher extends UNL_Services_Cou
 
     function getQueryResult($query, $offset = 0, $limit = null)
     {
-        $query =  'SELECT courses.xml FROM courses INNER JOIN crosslistings ON courses.id=crosslistings.course_id WHERE ' . $query . ';';
+        $query =  'SELECT courses.xml FROM courses INNER JOIN crosslistings ON courses.id=crosslistings.course_id WHERE crosslistings.courseNumber < "500" AND (' . $query . ');';
 
         return new UNL_UndergraduateBulletin_CourseSearch_DBSearchResults($query, $offset, $limit);
     }
