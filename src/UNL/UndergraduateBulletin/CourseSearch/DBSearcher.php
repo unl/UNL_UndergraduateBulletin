b95d3ae5b4602521b63cb6d09ac2014ddb0a441f
Brett Bieber <brett.bieber@gmail.com>
2012-04-15 09:40:33 -0500

    Add DISTINCT to course search SQL

--- src/UNL/UndergraduateBulletin/CourseSearch/DBSearcher.php
+++ src/UNL/UndergraduateBulletin/CourseSearch/DBSearcher.php
@@ -111,7 +111,7 @@ class UNL_UndergraduateBulletin_CourseSearch_DBSearcher extends UNL_Services_Cou
 
     function getQueryResult($query, $offset = 0, $limit = null)
     {
-        $query =  'SELECT courses.xml FROM courses INNER JOIN crosslistings ON courses.id=crosslistings.course_id WHERE crosslistings.courseNumber < "500" AND (' . $query . ');';
+        $query =  'SELECT DISTINCT courses.id, courses.xml FROM courses INNER JOIN crosslistings ON courses.id=crosslistings.course_id WHERE crosslistings.courseNumber < "500" AND (' . $query . ');';
 
         return new UNL_UndergraduateBulletin_CourseSearch_DBSearchResults($query, $offset, $limit);
     }
