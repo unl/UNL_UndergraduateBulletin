b54dabbfb9aec0ea7bba5aeb9cc4d6bae839e9b1
Brett Bieber <brett.bieber@gmail.com>
2012-04-30 08:10:14 -0500

    Use URL provided by the controller, nothing hardcoded

--- www/templates/json/SubjectAreas.tpl.php
+++ www/templates/json/SubjectAreas.tpl.php
@@ -2,7 +2,7 @@
 $subjects = array();
 foreach ($context as $subject_code => $area) {
     $subjects[$subject_code] = array(
-        'href' => 'http://'.$_SERVER['SERVER_NAME'].UNL_UndergraduateBulletin_Controller::getURL().'courses/'.$subject_code.'/',
+        'href'  => UNL_UndergraduateBulletin_Controller::getURL().'courses/'.$subject_code.'/',
         'title' => $area->getRaw('title')
     );
 }
