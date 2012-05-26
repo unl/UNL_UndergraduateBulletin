887702ac4ff8b9c7b3e1139822a9f06dcd96c612
Brett Bieber <brett.bieber@gmail.com>
2012-05-26 12:58:29 -0500

    When rendering the HTML, hide un-advertised subject codes

--- www/templates/html/SubjectAreas.tpl.php
+++ www/templates/html/SubjectAreas.tpl.php
@@ -1,8 +1,8 @@
 <h2>All subject areas</h2>
 <ul id="subjectListing">
 <?php
-foreach ($context as $subject_code => $area) {
-    echo '<li><a href="'.UNL_UndergraduateBulletin_Controller::getURL().'courses/'.$subject_code.'/"><span class="subjectCode">'.$subject_code.'</span> <span class="title">'.$area->title.'</span></a></li>';
-}
-?>
+$filter = Savvy_ObjectProxy::factory(new UNL_UndergraduateBulletin_SubjectAreas_Filter($context->getRawObject()), $savvy);
+foreach ($filter as $subject_code => $area) : ?>
+    <li><a href="<?php echo UNL_UndergraduateBulletin_Controller::getURL().'courses/'.$subject_code.'/'; ?>"><span class="subjectCode"><?php echo $subject_code; ?></span> <span class="title"><?php echo $area->title; ?></span></a></li>
+<?php endforeach; ?>
 </ul>
