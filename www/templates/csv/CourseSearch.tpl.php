81687c08184fa275bab686afaab25c3e722f2555
Michael Fairchild <mfairchild365@gmail.com>
2012-09-27 13:31:52 -0500

    Output all data related to a course.

--- www/templates/csv/CourseSearch.tpl.php
+++ www/templates/csv/CourseSearch.tpl.php
@@ -4,26 +4,115 @@ if (gettype($context->results) == 'string') {
     return;
 }
 
+
+//$group by course code
+$courses = array();
 $i = 0;
 foreach ($context->results as $course) {
-    $csvCourse = array();
-    $csvCourse['title'] = $course->title;
-    $csvCourse['description'] = $course->description;
-    $csvCourse['prerequisite'] = $course->prerequisite;
-    
-    //handle Codes
-    $codes = array();
     foreach ($course->codes as $listing) {
-        $codes[] = (string)$listing->subjectArea . " " . (string)$listing->courseNumber;
-    }
+       
+        $csvCourse                 = array();
+        $csvCourse['courseCode']   = (string)$listing->subjectArea . " " . (string)$listing->courseNumber;
+        $csvCourse['title']        = $course->title;
+        $csvCourse['description']  = $course->description;
+        $csvCourse['prerequisite'] = $course->prerequisite;
 
-    $csvCourse['courseCodes'] = implode(",", $codes);
-    
-    if ($i == 0) {
-        $delimitArray($delimiter, array_keys($csvCourse));
+        //Credits
+        $csvCourse['creditsSingleValue']   = $course->credits['Single Value'];
+        $csvCourse['creditsLowerLimit']    = $course->credits['Lower Range Limit'];
+        $csvCourse['creditsUpperLimit']    = $course->credits['Upper Range Limit'];
+        $csvCourse['creditsSemesterLimit'] = $course->credits['Per Semester Limit'];
+        $csvCourse['creditsCareerLimit']   = $course->credits['Per Career Limit'];
+        $csvCourse['dfRemoval']            = $course->getDFRemoval();
+        
+        //Terms
+        $possibleTerms = array('Fall', 'Spring', 'Summer');
+        foreach ($possibleTerms as $term) {
+            $csvCourse['term' . $term] = "";
+        }
+        
+        if (!empty($course->termsOffered)) {
+            foreach ($course->termsOffered as $term) {
+                $term = ucfirst($term);
+                if (!in_array($term, $possibleTerms)) {
+                    continue;
+                }
+
+                $csvCourse['term' . $term] = true;
+            }
+        }
+        
+        //deliveryMethods
+        $possibleDeliveries = array('Classroom', 'Web', 'Correspondence');
+        foreach ($possibleDeliveries as $delivery) {
+            $csvCourse['delivery' . $delivery] = "";
+        }
+        
+        if (!empty($course->deliveryMethods)) {
+            foreach ($course->deliveryMethods as $method) {
+                $method = ucfirst($method);
+                if (!in_array($method, $possibleDeliveries)) {
+                    continue;
+                }
+                
+                $csvCourse['delivery' . $method] = true;
+            }
+        }
+        
+        //Campuses
+        $possibleCampuses = array("UNL", "UNO", "UNMC", "UNK");
+        foreach ($possibleCampuses as $campus) {
+            $csvCourse['campus' . $campus] = "";
+        }
+        
+        if (!empty($course->campuses)) {
+            foreach ($course->campuses as $campus) {
+                $campus = strtoupper($campus);
+                if (!in_array($campus, $possibleCampuses)) {
+                    continue;
+                }
+                
+                $csvCourse['campus' . $campus] = true;
+            }
+        }
+        
+        //Ace outcomes
+        $possibleACE = range(1,10);
+        foreach ($possibleACE as $ACE) {
+            $csvCourse['ace' . $ACE] = "";
+        }
+        
+        if (!empty($course->aceOutcomes)) {
+            foreach ($course->aceOutcomes as $outcome) {
+                if (!in_array($outcome, $possibleACE)) {
+                    continue;
+                }
+                
+                $csvCourse['ACE' . $outcome] = true;
+            }
+        }
+        
+        //Activities
+        $possibleActivities = array('Lec', 'Lab', 'Stu', 'Fld', 'Quz', 'Rct', 'Ind', 'Psi');
+        foreach ($possibleActivities as $act) {
+            $csvCourse['activity' . $act] = "";
+        }
+        
+        foreach ($course->activities as $activity) {
+            $type = ucfirst($activity->type);
+            if (!in_array($type, $possibleActivities)) {
+                continue;
+            }
+
+            $csvCourse['activity' . $type] = true;
+        }
+
+        if ($i == 0) {
+            $delimitArray($delimiter, array_keys($csvCourse));
+        }
+
+        $delimitArray($delimiter, $csvCourse);
+        
+        $i++;
     }
-    
-    $delimitArray($delimiter, $csvCourse);
-    
-    $i++;
 }
