e3a5f5afa51b2ae0b34aeadcd4bbd056e4c1b9ff
Michael Fairchild <mfairchild365@gmail.com>
2012-09-27 15:24:36 -0500

    Only loop over what we have to, and get possible values from an outside source.

--- www/templates/csv/CourseSearch.tpl.php
+++ www/templates/csv/CourseSearch.tpl.php
@@ -9,103 +9,114 @@ if (gettype($context->results) == 'string') {
 $courses = array();
 $i = 0;
 foreach ($context->results as $course) {
-    foreach ($course->codes as $listing) {
-       
-        $csvCourse                 = array();
-        $csvCourse['courseCode']   = (string)$listing->subjectArea . " " . (string)$listing->courseNumber;
-        $csvCourse['title']        = $course->title;
-        $csvCourse['description']  = $course->description;
-        $csvCourse['prerequisite'] = $course->prerequisite;
+    $csvCourse                 = array();
+    $csvCourse['courseCode']   = ""; //Leave empty for now...
+    $csvCourse['title']        = $course->title;
+    $csvCourse['description']  = $course->description;
+    $csvCourse['prerequisite'] = $course->prerequisite;
 
-        //Credits
-        $csvCourse['creditsSingleValue']   = $course->credits['Single Value'];
-        $csvCourse['creditsLowerLimit']    = $course->credits['Lower Range Limit'];
-        $csvCourse['creditsUpperLimit']    = $course->credits['Upper Range Limit'];
-        $csvCourse['creditsSemesterLimit'] = $course->credits['Per Semester Limit'];
-        $csvCourse['creditsCareerLimit']   = $course->credits['Per Career Limit'];
-        $csvCourse['dfRemoval']            = $course->getDFRemoval();
-        
-        //Terms
-        $possibleTerms = array('Fall', 'Spring', 'Summer');
-        foreach ($possibleTerms as $term) {
-            $csvCourse['term' . $term] = "";
-        }
-        
-        if (!empty($course->termsOffered)) {
-            foreach ($course->termsOffered as $term) {
-                $term = ucfirst($term);
-                if (!in_array($term, $possibleTerms)) {
-                    continue;
-                }
+    //Credits
+    $csvCourse['creditsSingleValue']   = $course->credits['Single Value'];
+    $csvCourse['creditsLowerLimit']    = $course->credits['Lower Range Limit'];
+    $csvCourse['creditsUpperLimit']    = $course->credits['Upper Range Limit'];
+    $csvCourse['creditsSemesterLimit'] = $course->credits['Per Semester Limit'];
+    $csvCourse['creditsCareerLimit']   = $course->credits['Per Career Limit'];
+    $csvCourse['dfRemoval']            = $course->getDFRemoval();
 
-                $csvCourse['term' . $term] = true;
-            }
-        }
+    //Terms
+    $possibleTerms = UNL_Services_CourseApproval_Course::getPossibleTermsOffered();
+    $possibleTerms = array_keys($possibleTerms);
         
-        //deliveryMethods
-        $possibleDeliveries = array('Classroom', 'Web', 'Correspondence');
-        foreach ($possibleDeliveries as $delivery) {
-            $csvCourse['delivery' . $delivery] = "";
-        }
-        
-        if (!empty($course->deliveryMethods)) {
-            foreach ($course->deliveryMethods as $method) {
-                $method = ucfirst($method);
-                if (!in_array($method, $possibleDeliveries)) {
-                    continue;
-                }
-                
-                $csvCourse['delivery' . $method] = true;
-            }
-        }
-        
-        //Campuses
-        $possibleCampuses = array("UNL", "UNO", "UNMC", "UNK");
-        foreach ($possibleCampuses as $campus) {
-            $csvCourse['campus' . $campus] = "";
-        }
-        
-        if (!empty($course->campuses)) {
-            foreach ($course->campuses as $campus) {
-                $campus = strtoupper($campus);
-                if (!in_array($campus, $possibleCampuses)) {
-                    continue;
-                }
-                
-                $csvCourse['campus' . $campus] = true;
-            }
-        }
-        
-        //Ace outcomes
-        $possibleACE = range(1,10);
-        foreach ($possibleACE as $ACE) {
-            $csvCourse['ace' . $ACE] = "";
-        }
-        
-        if (!empty($course->aceOutcomes)) {
-            foreach ($course->aceOutcomes as $outcome) {
-                if (!in_array($outcome, $possibleACE)) {
-                    continue;
-                }
-                
-                $csvCourse['ACE' . $outcome] = true;
-            }
-        }
-        
-        //Activities
-        $possibleActivities = array('Lec', 'Lab', 'Stu', 'Fld', 'Quz', 'Rct', 'Ind', 'Psi');
-        foreach ($possibleActivities as $act) {
-            $csvCourse['activity' . $act] = "";
-        }
-        
-        foreach ($course->activities as $activity) {
-            $type = ucfirst($activity->type);
-            if (!in_array($type, $possibleActivities)) {
+    foreach ($possibleTerms as $term) {
+        $csvCourse['term' . $term] = "";
+    }
+
+    if (!empty($course->termsOffered)) {
+        foreach ($course->termsOffered as $term) {
+            $term = ucfirst($term);
+            if (!in_array($term, $possibleTerms)) {
                 continue;
             }
 
-            $csvCourse['activity' . $type] = true;
+            $csvCourse['term' . $term] = true;
         }
+    }
+
+    //deliveryMethods
+    $possibleDeliveries = UNL_Services_CourseApproval_Course::getPossibleDeliveryMethods();
+    $possibleDeliveries = array_keys($possibleDeliveries);
+    
+    foreach ($possibleDeliveries as $delivery) {
+        $csvCourse['delivery' . $delivery] = "";
+    }
+
+    if (!empty($course->deliveryMethods)) {
+        foreach ($course->deliveryMethods as $method) {
+            $method = ucfirst($method);
+            if (!in_array($method, $possibleDeliveries)) {
+                continue;
+            }
+
+            $csvCourse['delivery' . $method] = true;
+        }
+    }
+
+    //Campuses
+    $possibleCampuses = UNL_Services_CourseApproval_Course::getPossibleCampuses();
+    $possibleCampuses = array_keys($possibleCampuses);
+    
+    foreach ($possibleCampuses as $campus) {
+        $csvCourse['campus' . $campus] = "";
+    }
+
+    if (!empty($course->campuses)) {
+        foreach ($course->campuses as $campus) {
+            $campus = strtoupper($campus);
+            if (!in_array($campus, $possibleCampuses)) {
+                continue;
+            }
+
+            $csvCourse['campus' . $campus] = true;
+        }
+    }
+
+    //Ace outcomes
+    $possibleACE = UNL_Services_CourseApproval_Course::getPossibleAceOutcomes();
+    $possibleACE = array_keys($possibleACE);
+    
+    foreach ($possibleACE as $ACE) {
+        $csvCourse['ace' . $ACE] = "";
+    }
+
+    if (!empty($course->aceOutcomes)) {
+        foreach ($course->aceOutcomes as $outcome) {
+            if (!in_array((int)$outcome, $possibleACE)) {
+                continue;
+            }
+            
+            $csvCourse['ace' . $outcome] = true;
+        }
+    }
+
+    //Activities
+    $possibleActivities = UNL_Services_CourseApproval_Course::getPossibleActivities();
+    $possibleActivities = array_keys($possibleActivities);
+    
+    foreach ($possibleActivities as $act) {
+        $csvCourse['activity' . ucfirst($act)] = "";
+    }
+
+    foreach ($course->activities as $activity) {
+        $type = strtolower($activity->type);
+        if (!in_array($type, $possibleActivities)) {
+            continue;
+        }
+
+        $csvCourse['activity' . ucfirst($type)] = true;
+    }
+    
+    foreach ($course->codes as $listing) {
+        $csvCourse['courseCode'] = (string)$listing->subjectArea . " " . (string)$listing->courseNumber;
 
         if ($i == 0) {
             $delimitArray($delimiter, array_keys($csvCourse));
