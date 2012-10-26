c45d36dc2df811f777ec614d52ca0e1e2b9e7812
Michael Fairchild <mfairchild365@gmail.com>
2012-10-26 15:48:52 -0500

    Readability fix

--- www/templates/collegesource/CourseSearch.tpl.php
+++ www/templates/collegesource/CourseSearch.tpl.php
@@ -8,20 +8,19 @@ $subjectAreas = new UNL_UndergraduateBulletin_SubjectAreas();
 
 //$group by course code
 $courses = array();
+
 $i = 0;
 foreach ($context->results as $course) {
-    $csvCourse                 = array();
-    
-    $csvCourse['Department']   = "";
-    $csvCourse['DepartmentAbbr']   = "";
-    
-    $csvCourse['CourseCode']   = ""; //Leave empty for now...
-    $csvCourse['CourseLabel']  = $course->title;
-    $csvCourse['Description']  = strip_tags(html_entity_decode($course->description));
-    $csvCourse['Prerequisite'] = strip_tags(html_entity_decode($course->prerequisite));
+    $csvCourse                   = array();
+    $csvCourse['Department']     = "";
+    $csvCourse['DepartmentAbbr'] = "";
+    $csvCourse['CourseCode']     = ""; //Leave empty for now...
+    $csvCourse['CourseLabel']    = $course->title;
+    $csvCourse['Description']    = strip_tags(html_entity_decode($course->description));
+    $csvCourse['Prerequisite']   = strip_tags(html_entity_decode($course->prerequisite));
 
     //Credits
-    $csvCourse['Units']                = $course->credits['Single Value'];
+    $csvCourse['Units'] = $course->credits['Single Value'];
     
     foreach ($course->codes as $listing) {
         $subject = (string)$listing->subjectArea;
@@ -30,9 +29,9 @@ foreach ($context->results as $course) {
             continue;
         }
 
-        $csvCourse['Department'] = $subjectAreas[$subject];
+        $csvCourse['Department']     = $subjectAreas[$subject];
         $csvCourse['DepartmentAbbr'] = $subject;
-        $csvCourse['CourseCode'] = $subject . " " . (string)$listing->courseNumber;
+        $csvCourse['CourseCode']     = $subject . " " . (string)$listing->courseNumber;
 
         if ($i == 0) {
             $delimitArray($delimiter, array_keys($csvCourse));
