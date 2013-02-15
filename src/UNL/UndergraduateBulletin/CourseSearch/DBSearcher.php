dce22086c857bc5ab9bee2ceb2fa8f9807fccf6c
Brett Bieber <brett.bieber@gmail.com>
2013-02-15 11:11:44 -0600

    Readability improvements

--- src/UNL/UndergraduateBulletin/CourseSearch/DBSearcher.php
+++ src/UNL/UndergraduateBulletin/CourseSearch/DBSearcher.php
@@ -111,7 +111,14 @@ class UNL_UndergraduateBulletin_CourseSearch_DBSearcher extends UNL_Services_Cou
 
     function getQueryResult($query, $offset = 0, $limit = -1)
     {
-        $query =  'SELECT DISTINCT courses.id, courses.xml FROM courses INNER JOIN crosslistings ON courses.id=crosslistings.course_id WHERE (LENGTH(crosslistings.courseNumber) = 3 AND crosslistings.courseNumber < "500" OR LENGTH(crosslistings.courseNumber) < 3) AND (' . $query . ');';
+        $query =  'SELECT DISTINCT courses.id, courses.xml 
+                   FROM courses INNER JOIN crosslistings ON courses.id=crosslistings.course_id
+                   WHERE (
+                          LENGTH(crosslistings.courseNumber) = 3
+                          AND crosslistings.courseNumber < "500"
+                          OR LENGTH(crosslistings.courseNumber) < 3
+                         )
+                         AND (' . $query . ');';
         return new UNL_UndergraduateBulletin_CourseSearch_DBSearchResults($query, $offset, $limit);
     }
 }
