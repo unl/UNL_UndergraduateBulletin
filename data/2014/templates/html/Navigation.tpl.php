f0ddb5ba372e440abda0961d0c5ca29756220bdd
Brett Bieber <brett.bieber@gmail.com>
2013-07-23 13:50:52 -0500

    Prepare workspace for 2014 edition, using 2013 as the base

---
+++ data/2014/templates/html/Navigation.tpl.php
@@ -0,0 +1,55 @@
+<?php
+$url = UNL_UndergraduateBulletin_Controller::getURL();
+?>
+<ul>
+    <li><a href="<?php echo $url; ?>major/">Majors</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>major/search">Search for a Major</a></li>
+        </ul>
+    </li>
+    <li><a href="<?php echo $url; ?>courses/">Courses</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>courses/search">Search for a Course</a></li>
+            <li><a href="<?php echo $url; ?>courses/">Course Abbreviations</a></li>
+        </ul>
+    </li>
+    <li><a href="<?php echo $url; ?>general">Academic Policies</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>general#admission-categories">Admission Categories</a></li>
+            <li><a href="<?php echo $url; ?>general#undergraduate-transfer-credit-policy">Undergraduate Transfer Credit Policy</a></li>
+            <li><a href="<?php echo $url; ?>general#graduation-requirements">Graduation Requirements</a></li>
+            <li><a href="<?php echo $url; ?>general#academic-policies-and-procedures">Academic Policies and Procedures</a></li>
+            <li><a href="<?php echo $url; ?>general#glossary-of-academic-and-advising-terms">Glossary of Academic and Advising Terms</a></li>
+            <li><a href="http://stuafs.unl.edu/ja/code" title="Go to the Student Code of Conduct">Student Code of Conduct</a></li>
+        </ul>
+    </li>
+    <li><a href="<?php echo $url; ?>college/">Academic Colleges</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>college/Agricultural+Sciences+%26+Natural+Resources">Agricultural Sciences &amp; Natural Resources</a></li>
+            <li><a href="<?php echo $url; ?>college/Architecture">Architecture</a></li>
+            <li><a href="<?php echo $url; ?>college/Arts+%26+Sciences">Arts &amp; Sciences</a></li>
+            <li><a href="<?php echo $url; ?>college/Business+Administration">Business Administration</a></li>
+            <li><a href="<?php echo $url; ?>college/Education+%26+Human+Sciences">Education &amp; Human Sciences</a></li>
+            <li><a href="<?php echo $url; ?>college/Engineering">Engineering</a></li>
+            <li><a href="<?php echo $url; ?>college/Fine+%26+Performing+Arts">Hixson-Lied College of Fine &amp; Performing Arts</a></li>
+            <li><a href="<?php echo $url; ?>college/Journalism+%26+Mass+Communications">Journalism &amp; Mass Communications</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="<?php echo $url; ?>college/Exploratory+%26+Pre-Professional+Advising+Center">Explore Center</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>college/Exploratory+%26+Pre-Professional+Advising+Center#undecided-students">Undecided Students</a></li>
+            <li><a href="<?php echo $url; ?>college/Exploratory+%26+Pre-Professional+Advising+Center#pre-professional-students">Pre-Professional Programs</a></li>
+        </ul>
+    </li>
+    <li><a href="<?php echo $url; ?>college/">Other Academic Units</a>
+        <ul>
+            <li><a href="<?php echo $url; ?>college/Undergraduate+Education#general-education-requirements-ace">General Education (ACE)</a></li>
+            <li><a href="<?php echo $url; ?>college/Undergraduate+Education#jeffrey-s-raikes-school-of-computer-science-and-ma">Jeffrey S. Raikes School of Computer Science and Management</a></li>
+            <li><a href="<?php echo $url; ?>college/Libraries">Libraries</a></li>
+            <li><a href="<?php echo $url; ?>college/Public+Affairs+%26+Community+Service">Public Affairs &amp; Community Service</a></li>
+            <li><a href="<?php echo $url; ?>college/Reserve+Officers+Training+Corps+%28ROTC%29">Reserve Officers Training Corps (ROTC)</a></li>
+            <li><a href="<?php echo $url; ?>college/Undergraduate+Education#university-honors-program-">University Honors</a></li>
+        </ul>
+    </li>
+</ul>
