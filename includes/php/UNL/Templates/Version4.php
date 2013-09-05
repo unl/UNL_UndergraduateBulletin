b88c0af702b4e7777e01d72008b63bdbadfbc90d
Brett Bieber <brett.bieber@gmail.com>
2013-09-05 08:18:26 -0500

    Upgrade template library

--- includes/php/UNL/Templates/Version4.php
+++ includes/php/UNL/Templates/Version4.php
@@ -24,42 +24,40 @@ require_once 'UNL/Templates/Version.php';
  * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License
  * @link      http://pear.unl.edu/
  */
-class UNL_Templates_Version3x1 implements UNL_Templates_Version
+class UNL_Templates_Version4 implements UNL_Templates_Version
 { 
     function getConfig()
     {
-        return array('class_location' => 'UNL/Templates/Version3x1/',
-                     'class_prefix'   => 'UNL_Templates_Version3x1_');
+        return array('class_location' => 'UNL/Templates/Version4/',
+                     'class_prefix'   => 'UNL_Templates_Version4_');
     }
-    
+
     function getTemplate($template)
     {
-        if (!file_exists(UNL_Templates::$options['templatedependentspath'].'/wdn/templates_3.1')) {
-            UNL_Templates::debug('ERROR You should have a local copy of wdn/templates_3.1!'
-                                 . ' Overriding your specified template to use absolute references' ,
-                                 'getTemplate', 1);
-            $template = 'Local.tpl';
-        }
+
+        // Set a timeout for the HTTP download of the template file
+        $http_context = stream_context_create(array('http' => array('timeout' => UNL_Templates::$options['timeout'])));
 
         // Always try and retrieve the latest
         if (!(UNL_Templates::getCachingService() instanceof UNL_Templates_CachingService_Null)
-            && $tpl = file_get_contents('http://pear.unl.edu/UNL/Templates/server.php?version=3x1&template='.$template)) {
+            && $tpl = file_get_contents('https://raw.github.com/unl/wdntemplates/4.0/'.$template, false, $http_context)) {
             return $tpl;
         }
 
-        if (file_exists(UNL_Templates::getDataDir().'/tpl_cache/Version3x1/'.$template)) {
-            return file_get_contents(UNL_Templates::getDataDir().'/tpl_cache/Version3x1/'.$template);
+        if ($tpl = file_get_contents('https://raw.github.com/unl/wdntemplates/4.0/'.$template)) {
+            return $tpl;
         }
+        
 
         throw new Exception('Could not get the template file!');
     }
-
+    
     function makeIncludeReplacements($html)
     {
         UNL_Templates::debug('Now making template include replacements.',
                      'makeIncludeReplacements', 3);
         $includes = array();
-        preg_match_all('<!--#include virtual="(/wdn/templates_3.1/[A-Za-z0-9\.\/_]+)" -->',
+        preg_match_all('<!--#include virtual="(/wdn/templates_4.0/[A-Za-z0-9\.\/_]+)" -->',
                         $html, $includes);
         UNL_Templates::debug(print_r($includes, true), 'makeIncludeReplacements', 3);
         foreach ($includes[1] as $include) {
@@ -68,7 +66,7 @@ class UNL_Templates_Version3x1 implements UNL_Templates_Version
             if (!file_exists($file)) {
                 UNL_Templates::debug('File does not exist:'.$file,
                              'makeIncludeReplacements', 3);
-                $file = 'http://www.unl.edu'.$include;
+                $file = 'https://raw.github.com/unl/wdntemplates/4.0'.$include;
             }
             $html = str_replace('<!--#include virtual="'.$include.'" -->',
                                  file_get_contents($file), $html);
