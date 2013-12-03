e53758ff9b56b9b1bb07ae01066d4d93f6e37687
Brett Bieber <brett.bieber@gmail.com>
2013-12-03 13:56:05 -0600

    Wording improvements on course search options

--- www/templates/html/CourseSearchForm.tpl.php
+++ www/templates/html/CourseSearchForm.tpl.php
@@ -7,10 +7,11 @@
     </fieldset>
     <div class="search_help">
         <p>
-        Sample: Course Title (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=Global+Advertising">Global Advertising</a>),
-        Course Code (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=ACCT">ACCT</a>),
-        Course Number (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=438">438</a>),
-        Ace Outcome (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=ace+3">ace 3</a>) Course Number Range (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=2*">2*</a>),
+        Samples: Title (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=Global+Advertising">Global Advertising</a>),
+        Subject Code (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=ACCT">ACCT</a>),
+        Number (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=438">438</a>),
+        Number Range (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=2*">2*</a>),
+        Ace Outcome (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=ace+3">ace 3</a>),
         Honors Courses (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=honors">honors</a>),
         Credit Hours (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=2+credits">2 credits</a>),
         or combine terms (<a class="operator" href="<?php echo UNL_UndergraduateBulletin_Controller::getURL(); ?>courses/search?q=ace+3+CSCE+honors">ace 3 CSCE honors</a>)
