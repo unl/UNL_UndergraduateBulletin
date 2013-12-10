0a017feb56cd2b4688e8ed94489b1a1ad98e8f9d
Brett Bieber <brett.bieber@gmail.com>
2013-12-10 16:40:28 -0600

    Add wdn-course-year class to each year wrapper

--- www/templates/html/Major/FourYearPlan/Concentration.tpl.php
+++ www/templates/html/Major/FourYearPlan/Concentration.tpl.php
@@ -2,7 +2,7 @@
 foreach ($context as $count => $semester) {
     $savvy->addGlobal('semester', $count);
     ?>
-    <div class="wdn-col">
+    <div class="wdn-col wdn-course-year">
 		<h3 class="wdn-center wdn-course-year-header"><span class="wdn-course-year-header-year">Year</span> <span class="wdn-course-year-header-number"><?php echo ($count+1)/2; ?></span></h3>
 		<div class="bp2-wdn-grid-set-halves wdn-grid-clear">
 		<?php
