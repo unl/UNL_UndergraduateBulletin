bd2ead117e313dad2953d11bca6bf91905369e22
Brett Bieber <brett.bieber@gmail.com>
2014-02-15 10:17:40 -0600

    Add a model for the collection of other areas of content

---
+++ src/UNL/UndergraduateBulletin/OtherAreas.php
@@ -0,0 +1,32 @@
+<?php
+class UNL_UndergraduateBulletin_OtherAreas extends ArrayIterator  implements UNL_UndergraduateBulletin_CacheableInterface
+{
+    public $options = array('format'=>'html');
+
+    function __construct($options = array())
+    {
+        $this->options = $options + $this->options;
+        $otherareas = glob(UNL_UndergraduateBulletin_Controller::getEdition()->getDataDir().'/other/*.xhtml');
+        return parent::__construct($otherareas);
+    }
+
+    function getCacheKey()
+    {
+        return 'otherlist'.$this->options['format'];
+    }
+
+    function run()
+    {
+        
+    }
+
+    function preRun()
+    {
+        
+    }
+
+    function current()
+    {
+        return new UNL_UndergraduateBulletin_OtherArea(array('name'=>UNL_UndergraduateBulletin_OtherArea_Description::getNameByFile(parent::current())));
+    }
+}
