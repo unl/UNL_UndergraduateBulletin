e01ae397f6c3bd680f82e9ad3527e186eed100a8
Brett Bieber <brett.bieber@gmail.com>
2014-02-17 21:50:35 -0600

    Whitespace, tabs to spaces

--- www/templates/html/SearchForm.tpl.php
+++ www/templates/html/SearchForm.tpl.php
@@ -8,12 +8,12 @@ if ($controller->options['view'] == 'searchmajors') {
 }
 ?>
 <div class="wdn-inner-wrapper">
-	<div id="search_forms">
-	    <h3 id="search_label">Find a <span class="option <?php echo $course_search; ?>" id="course" tabindex="0">Course</span> or find a <span class="option <?php echo $major_search; ?>" id="major" tabindex="0">Major/Degree</span>
-	    </h3>
-	    <?php
-	    echo $savvy->render('', 'CourseSearchForm.tpl.php');
-	    echo $savvy->render('', 'MajorList/SearchForm.tpl.php');
-	    ?>
-	</div>
+    <div id="search_forms">
+        <h3 id="search_label">Find a <span class="option <?php echo $course_search; ?>" id="course" tabindex="0">Course</span> or find a <span class="option <?php echo $major_search; ?>" id="major" tabindex="0">Major/Degree</span>
+        </h3>
+        <?php
+        echo $savvy->render('', 'CourseSearchForm.tpl.php');
+        echo $savvy->render('', 'MajorList/SearchForm.tpl.php');
+        ?>
+    </div>
 </div>
