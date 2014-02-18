c9ed55e05f625fe47fb5c8eab79490587c6ceb9d
Brett Bieber <brett.bieber@gmail.com>
2014-02-17 21:46:50 -0600

    Ensure major search option is active on major search page

--- www/templates/html/SearchForm.tpl.php
+++ www/templates/html/SearchForm.tpl.php
@@ -1,6 +1,15 @@
+<?php
+$course_search = 'active';
+$major_search  = '';
+
+if ($controller->options['view'] == 'searchmajors') {
+    $course_search = '';
+    $major_search  = 'active';
+}
+?>
 <div class="wdn-inner-wrapper">
 	<div id="search_forms">
-	    <h3 id="search_label">Find a <span class="option active" id="course" tabindex="0">Course</span> or find a <span class="option" id="major" tabindex="0">Major/Degree</span>
+	    <h3 id="search_label">Find a <span class="option <?php echo $course_search; ?>" id="course" tabindex="0">Course</span> or find a <span class="option <?php echo $major_search; ?>" id="major" tabindex="0">Major/Degree</span>
 	    </h3>
 	    <?php
 	    echo $savvy->render('', 'CourseSearchForm.tpl.php');
