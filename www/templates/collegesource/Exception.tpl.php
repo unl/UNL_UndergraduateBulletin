4c91668c99f6e5356919764804cd48a31040a06c
Kevin Abel <kevin.abel.0@gmail.com>
2014-07-02 15:17:44 -0500

    Add exception templates for the CSV formats

---
+++ www/templates/collegesource/Exception.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/* @var $context Exception */
+if (false == headers_sent()
+    && $code = $context->getCode()) {
+    header('HTTP/1.1 '.$code.' '.$context->getMessage());
+    header('Status: '.$code.' '.$context->getMessage());
+}
+?>
