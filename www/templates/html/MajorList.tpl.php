e929dbc309d7efb92c6ec0cbae469bcf83893d08
Kevin Abel <kevin.abel.0@gmail.com>
2015-03-06 15:52:02 -0600

    Convert MajorList to use new wdn grid classes.

--- www/templates/html/MajorList.tpl.php
+++ www/templates/html/MajorList.tpl.php
@@ -1,7 +1,9 @@
-<div class="grid3 first">
-    <?php echo $savvy->render(null, 'MajorList/Filters.tpl.php'); ?>
-</div>
-<div class="grid9 majorlist2col">
-    <h2 class="clear-top">Select A Major or Area of Study</h2>
-    <?php echo $savvy->render($context, 'MajorList/UnorderedList.tpl.php'); ?>
+<div class="wdn-grid-set">
+    <div class="bp2-wdn-col-one-fourth">
+        <?php echo $savvy->render(null, 'MajorList/Filters.tpl.php'); ?>
+    </div>
+    <div class="bp2-wdn-col-three-fourths">
+        <h2 class="clear-top">Select A Major or Area of Study</h2>
+        <?php echo $savvy->render($context, 'MajorList/UnorderedList.tpl.php'); ?>
+    </div>
 </div>
