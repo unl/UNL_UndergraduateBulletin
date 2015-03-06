013f95c7ac81d87a5d3a456b2d8aa95c56c1e0d9
Kevin Abel <kevin.abel.0@gmail.com>
2015-03-06 15:51:11 -0600

    Remove inner wrapper from majorlist

--- www/templates/html/MajorList/UnorderedList.tpl.php
+++ www/templates/html/MajorList/UnorderedList.tpl.php
@@ -1,23 +1,21 @@
-<div class="wdn-inner-wrapper">
-    <ul id="majorListing" class="majorlisting">
-        <?php foreach ($context as $major):
-        $note = '';
-        $class = array();
-        if ($major->minorAvailable()) {
-            $class[] = 'minorAvailable';
-            $note = 'Minor&nbsp;Available';
-        }
-        
-        if ($major->minorOnly()) {
-            $class[] = 'minorOnly';
-            $note = 'Minor&nbsp;Only';
-        }
-        
-        foreach ($major->colleges as $college) {
-            $class[] = $college->abbreviation;
-        }
-        ?>
-        <li class="<?php echo implode(' ', $class); ?>"><a href="<?php echo $major->getRawObject()->getURL(); ?>"><?php echo $major->title; ?><?php if ($note): ?> <span class="note"><?php echo $note; ?></span><?php endif; ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-</div>
+<ul id="majorListing" class="majorlisting">
+    <?php foreach ($context as $major):
+    $note = '';
+    $class = array();
+    if ($major->minorAvailable()) {
+        $class[] = 'minorAvailable';
+        $note = 'Minor&nbsp;Available';
+    }
+    
+    if ($major->minorOnly()) {
+        $class[] = 'minorOnly';
+        $note = 'Minor&nbsp;Only';
+    }
+    
+    foreach ($major->colleges as $college) {
+        $class[] = $college->abbreviation;
+    }
+    ?>
+    <li class="<?php echo implode(' ', $class); ?>"><a href="<?php echo $major->getRawObject()->getURL(); ?>"><?php echo $major->title; ?><?php if ($note): ?> <span class="note"><?php echo $note; ?></span><?php endif; ?></a></li>
+    <?php endforeach; ?>
+</ul>
