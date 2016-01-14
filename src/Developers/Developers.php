c813f036f40287585d1117b26c680ee1fcb38b45
Kevin Abel <kabel2@unl.edu>
2016-01-14 10:20:17 -0600

    Move Developers class into namespace folder

--- src/Developers/Developers.php
+++ src/Developers/Developers.php
@@ -1,24 +1,27 @@
 <?php
-class UNL_UndergraduateBulletin_Developers
+
+namespace UNL\UndergraduateBulletin;
+
+class Developers
 {
     public $resources = array('Course', 'CollegeMajors', 'Colleges', 'SubjectArea', 'CourseSearch', 'FourYearPlans');
-    
+
     public $resource;
-    
+
     public $options = array();
-    
+
     function __construct($options = array())
     {
         $this->options  = $options;
         $this->resource = $this->resources[0];
-        
+
         if (isset($this->options['resource']) ) {
             if (in_array($this->options['resource'], $this->resources)) {
                 $this->resource = $this->options['resource'];
             }
         }
     }
-    
+
     static function formatJSON($json)
     {
         $replacements = array(
