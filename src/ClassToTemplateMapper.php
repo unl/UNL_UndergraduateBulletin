1a1a2b123c8d4e48abde9132542abdf826f69299
Kevin Abel <kabel2@unl.edu>
2016-01-13 19:28:43 -0600

    Refactor the class to template mapper

--- src/ClassToTemplateMapper.php
+++ src/ClassToTemplateMapper.php
@@ -1,12 +1,22 @@
 <?php
-class UNL_UndergraduateBulletin_ClassToTemplateMapper extends Savvy_ClassToTemplateMapper
+
+namespace UNL\UndergraduateBulletin;
+
+class ClassToTemplateMapper extends Savvy_ClassToTemplateMapper
 {
-    function map($class)
+	protected $removePrefixes = [
+		__NAMESPACE__ . '\\',
+		'UNL\\Service\\CourseApproval\\',
+	];
+
+    public function map($class)
     {
         if (isset(self::$output_template[$class])) {
             $class = self::$output_template[$class];
         }
-        $class = str_replace(array('UNL_UndergraduateBulletin_', 'UNL_Services_CourseApproval_'), '', $class);
+
+        $class = str_replace($this->removePrefixes, '', $class);
+
         return parent::map($class);
     }
 }
