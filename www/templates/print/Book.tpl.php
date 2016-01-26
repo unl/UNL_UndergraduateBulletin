27d147f977eec87f1c076caf364a0573502a7a59
Kevin Abel <kabel2@unl.edu>
2016-01-26 13:32:59 -0600

    Adjust namespace reference in print Book format

--- www/templates/print/Book.tpl.php
+++ www/templates/print/Book.tpl.php
@@ -1,18 +1,22 @@
-<h1>Undergraduate Bulletin <?php echo UNL_UndergraduateBulletin_Controller::getEdition()->getRange(); ?></h1>
+<?php
+use UNL\UndergraduateBulletin\Controller;
+use UNL\UndergraduateBulletin\EPUB\Utilities;
+?>
+<h1>Undergraduate Bulletin <?php echo Controller::getEdition()->getRange(); ?></h1>
 
 <?php foreach ($context->policies as $page): ?>
     <section class="general">
-    <?php echo UNL_UndergraduateBulletin_EPUB_Utilities::convertHeadings($page->getRawObject()->getDescription()); ?>
+        <?php echo Utilities::convertHeadings($page->getRawObject()->getDescription()); ?>
     </section>
 <?php endforeach; ?>
 
 <?php foreach ($context->colleges as $college): ?>
     <section class="college">
-    <?php echo UNL_UndergraduateBulletin_EPUB_Utilities::convertHeadings($college->getRawObject()->getDescription()); ?>
-    <?php foreach ($college->getRaw('majors') as $major): ?>
-        <section class="major">
-        <?php echo $major->getDescription()->description; ?>
-        </section>
-    <?php endforeach; ?>
+        <?php echo Utilities::convertHeadings($college->getRawObject()->getDescription()); ?>
+        <?php foreach ($college->getRaw('majors') as $major): ?>
+            <section class="major">
+                <?php echo $major->getDescription()->description; ?>
+            </section>
+        <?php endforeach; ?>
     </section>
 <?php endforeach; ?>
