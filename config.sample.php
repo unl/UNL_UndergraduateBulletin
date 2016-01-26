101b255285832ab692f002896c99eef53c4b1d0f
Kevin Abel <kabel2@unl.edu>
2016-01-26 15:33:35 -0600

    Adjust sample config to work with composer and namespace

--- config.sample.php
+++ config.sample.php
@@ -1,32 +1,36 @@
 <?php
-function autoload($class)
-{
-    $class = str_replace('_', '/', $class);
-    include $class . '.php';
-}
-    
-spl_autoload_register("autoload");
+
+use UNL\UndergraduateBulletin\Controller;
+use UNL\UndergraduateBulletin\OutputController;
+use UNL\UndergraduateBulletin\CourseDataDriver;
+use UNL\UndergraduateBulletin\CachingService\Mock;
+use UNL\UndergraduateBulletin\CachingService\UNLCacheLite;
+use UNL\UndergraduateBulletin\Course\DataDriver;
+use UNL\UndergraduateBulletin\Editions\Edition;
+use UNL\Services\CourseApproval\Data;
+use UNL\Services\CourseApproval\CachingService\NullService;
 
 // Set to false on production machines
 ini_set('display_errors', true);
 error_reporting(E_ALL);
 
-set_include_path(dirname(__FILE__).'/src'.PATH_SEPARATOR.dirname(__FILE__).'/includes/php');
-require_once 'UNL/Autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 // Set this to the web root for the site
-UNL_UndergraduateBulletin_Controller::$url = '/workspace/UNL_UndergraduateBulletin/www/';
+Controller::$url = '/workspace/UNL_UndergraduateBulletin/www/';
 
 // Remove this line on production machines so the default UNLCacheLite cache interface is used
-UNL_UndergraduateBulletin_OutputController::setCacheInterface(new UNL_UndergraduateBulletin_CacheInterface_Mock());
-UNL_UndergraduateBulletin_OutputController::setDefaultExpireTimestamp(strtotime('+1 week'));
+OutputController::setCacheInterface(new Mock());
+// OutputController::setCacheInterface(new UNLCacheLite(['cacheDir' => __DIR__ . '/tmp/']));
+OutputController::setDefaultExpireTimestamp(strtotime('+1 week'));
 
-//UNL_UndergraduateBulletin_CacheInterface_Mock::$logger = function($key) {
+
+// Mock::$logger = function($key) {
 //    $log = 'unlcache_5174748813ed8803e7651fae9d2d077f_'.md5($key).PHP_EOL;
 //    file_put_contents('/tmp/cachedfiles.txt', $log, FILE_APPEND);
-//};
+// };
 
-UNL_Services_CourseApproval::setCachingService(new UNL_Services_CourseApproval_CachingService_Null());
-UNL_Services_CourseApproval::setXCRIService(new UNL_UndergraduateBulletin_CourseDataDriver());
+Data::setCachingService(new NullService());
+Data::setXCRIService(new DataDriver());
 
-//UNL_UndergraduateBulletin_Controller::setEdition(new UNL_UndergraduateBulletin_Edition(array('year'=>'2010')));
+// Controller::setEdition(new Edition(['year' => '2010']));
