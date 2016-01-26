3d866e0dbe2fefaa107610582abca98cc7a0f793
Kevin Abel <kabel2@unl.edu>
2016-01-26 15:37:13 -0600

    Restore list template for displaying all majors

---
+++ www/templates/html/Major/Majors.tpl.php
@@ -0,0 +1,13 @@
+<div class="wdn-band">
+	<div class="wdn-inner-wrapper">
+		<div class="wdn-grid-set">
+		    <div class="bp2-wdn-col-one-fourth">
+		        <?php echo $savvy->render(null, 'Major/Filters.tpl.php'); ?>
+		    </div>
+		    <div class="bp2-wdn-col-three-fourths">
+		        <h2 class="clear-top">Select A Major or Area of Study</h2>
+		        <?php echo $savvy->render($context, 'Major/UnorderedList.tpl.php'); ?>
+		    </div>
+		</div>
+	</div>
+</div>
