bf4efef0aa6690f92127ff59fa04f4ecb7e8797e
Kevin Abel <kabel2@unl.edu>
2016-01-26 17:01:06 -0600

    Avoid double-encode XML title

--- www/templates/xml/Major/Lookup.tpl.php
+++ www/templates/xml/Major/Lookup.tpl.php
@@ -1,5 +1,5 @@
 <plans>
 <?php foreach ($context as $code => $title): ?>
-    <p xml:id="<?php echo $code ?>"><?php echo htmlentities($title) ?></p>
+    <p xml:id="<?php echo $code ?>"><?php echo $title ?></p>
 <?php endforeach; ?>
 </plans>
