036babb5f0b4aa5e38938fbab3621b5b7fcf7978
Kevin Abel <kabel2@unl.edu>
2016-01-26 18:34:05 -0600

    Add trait for json serializing an iterable object

---
+++ src/SelfIteratingJsonSerializationTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace UNL\UndergraduateBulletin;
+
+trait SelfIteratingJsonSerializationTrait
+{
+	public function jsonSerialize()
+    {
+        $items = [];
+        foreach ($this as $item) {
+            $items[] = $item;
+        }
+
+        return $items;
+    }
+}
