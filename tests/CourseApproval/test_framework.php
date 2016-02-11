64936496725f9cbce47b75d101cf367432f76ce0
Kevin Abel <kabel2@unl.edu>
2016-02-11 15:51:17 -0600

    Backport test fixes so we can start passing again.

--- tests/CourseApproval/test_framework.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once dirname(__FILE__).'/../test_framework.php.inc';
-
-UNL_Services_CourseApproval::setCachingService(new UNL_Services_CourseApproval_CachingService_Null());
-UNL_Services_CourseApproval::setXCRIService(new UNL_Services_CourseApproval_XCRIService_MockService());
-
