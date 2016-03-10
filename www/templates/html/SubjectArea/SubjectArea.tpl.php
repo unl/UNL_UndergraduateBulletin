b0b611004937b9acda175177d15ac31cb48ff097
Kevin Abel <kabel2@unl.edu>
2016-03-10 16:19:21 -0600

    Remove page related logic from SubjectArea template

--- www/templates/html/SubjectArea/SubjectArea.tpl.php
+++ www/templates/html/SubjectArea/SubjectArea.tpl.php
@@ -1,17 +1,3 @@
-<?php
-if (isset($parent->context->options) && $parent->context->options['view'] == 'subject') {
-    $url = $controller->getRawObject()::getURL();
-    $controller->getRawObject()::setReplacementData('doctitle', $context->getSubject() . ' Courses | Undergraduate Bulletin | University of Nebraska-Lincoln');
-    $controller->getRawObject()::setReplacementData('pagetitle', '<h1>'.$context->getSubject().' Courses</h1>');
-    $controller->getRawObject()::setReplacementData('breadcrumbs', '
-    <ul>
-        <li><a href="http://www.unl.edu/">UNL</a></li>
-        <li><a href="'.$url.'">Undergraduate Bulletin</a></li>
-        <li>'.$context->getSubject().'</li>
-    </ul>
-    ');
-}
-?>
 <div class="wdn-band course-list">
     <div class="wdn-inner-wrapper">
         <div class="wdn-grid-set">
