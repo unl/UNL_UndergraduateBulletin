1815025ea2751eb2a9e48f1baef30e4034ee8d85
Kevin Abel <kabel2@unl.edu>
2016-03-10 16:20:06 -0600

    Move page logic for SubjectAreas out of template

--- src/SubjectArea/SubjectAreas.php
+++ src/SubjectArea/SubjectAreas.php
@@ -3,12 +3,17 @@
 namespace UNL\UndergraduateBulletin\SubjectArea;
 
 use UNL\UndergraduateBulletin\Controller;
+use UNL\UndergraduateBulletin\CatalogController;
+use UNL\UndergraduateBulletin\ControllerAwareInterface;
 use UNL\UndergraduateBulletin\CachingService\CacheableInterface;
 
 class SubjectAreas extends \ArrayIterator implements
     CacheableInterface,
+    ControllerAwareInterface,
     \JsonSerializable
 {
+    protected $controller;
+
     /**
      * Returns an array with subject code keys and title values
      *
@@ -24,17 +29,45 @@ class SubjectAreas extends \ArrayIterator implements
         $mapping = file_get_contents($edition->getCourseDataDir() . '/subject_codes.php.ser');
 
         if (false === ($mapping = unserialize($mapping))) {
-            throw new Exception('Invalid major to subject code matching file.', 500);
+            throw new \Exception('Invalid major to subject code matching file.', 500);
         }
 
         return $mapping;
     }
 
-    public function __construct($options = array())
+    public function __construct($options = [])
     {
         $this->options = $options;
 
         parent::__construct(self::getMap());
+    }
+
+    public function setController(Controller $controller)
+    {
+        $page = $controller->getOutputPage();
+        $pageTitle = 'Subject Areas';
+
+        $titleContext = 'Undergraduate Bulletin';
+        if ($controller instanceof CatalogController) {
+            $titleContext = 'Course Catalog';
+            $page->breadcrumbs->addCrumb('Course Catalog', $controller::getURL());
+        }
+
+        $page->doctitle = sprintf(
+            '<title>%s | %s | University of Nebraska-Lincoln</title>',
+            $pageTitle,
+            $titleContext
+        );
+        $page->pagetitle = '<h1>' . $pageTitle . '</h1>';
+        $page->breadcrumbs->addCrumb($pageTitle);
+
+        $this->controller = $controller;
+        return $this;
+    }
+
+    public function getController()
+    {
+        return $this->controller;
     }
 
     public function getCacheKey()
@@ -59,6 +92,11 @@ class SubjectAreas extends \ArrayIterator implements
 
         $area = new SubjectArea($options);
         return $area;
+    }
+
+    public function getFiltered()
+    {
+        return new Filter($this);
     }
 
     public function jsonSerialize()
--- www/templates/html/SubjectArea/SubjectAreas.tpl.php
+++ www/templates/html/SubjectArea/SubjectAreas.tpl.php
@@ -1,13 +1,9 @@
 <div class="wdn-band">
-<div class="wdn-inner-wrapper">
-
-<h2 class="clear-top">All subject areas</h2>
-<ul id="subjectListing">
-<?php
-$filter = Savvy_ObjectProxy::factory(new UNL\UndergraduateBulletin\SubjectArea\Filter($context->getRawObject()), $savvy);
-foreach ($filter as $subject_code => $area) : ?>
-    <li><a href="<?php echo $controller->getRawObject()::getURL().'courses/'.$subject_code.'/'; ?>"><span class="subjectCode"><?php echo $subject_code; ?></span> <span class="title"><?php echo $area->title; ?></span></a></li>
-<?php endforeach; ?>
-</ul>
-</div>
+	<div class="wdn-inner-wrapper">
+		<ul id="subjectListing">
+			<?php foreach ($context->getFiltered() as $subject_code => $area) : ?>
+		    	<li><a href="<?php echo $area->getUrl($controller->getRawObject()) ?>"><span class="subjectCode"><?php echo $area->getSubject() ?></span> <span class="title"><?php echo $area->getTitle() ?></span></a></li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
 </div>
