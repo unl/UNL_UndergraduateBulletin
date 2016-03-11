efe0ecf02879354d63bc201897990fdf68ddf2d2
Kevin Abel <kabel2@unl.edu>
2016-03-11 12:15:27 -0600

    Use a sliding window pagination algorithm for rendering pages

--- www/templates/html/PaginationLinks.tpl.php
+++ www/templates/html/PaginationLinks.tpl.php
@@ -1,28 +1,118 @@
+<?php
+$currentPage = floor($context->offset / $context->limit) + 1;
+$totalPages = ceil($context->total / $context->limit);
+
+$adjacentPageCount = 2;
+$pageBookendCount = 2;
+$pageMinimum = 7;
+$pageWindow = $pageMinimum - $pageBookendCount - 1;
+?>
+
 <script>
 WDN.loadCSS(WDN.getTemplateFilePath('css/modules/pagination.css'));
 </script>
+
 <ul class="wdn_pagination">
-    <?php if ($context->offset != 0) :?>
-    <li class="arrow"><a href="<?php echo $context->url.'&amp;limit='.$context->limit.'&amp;offset='.($context->offset-$context->limit); ?>" title="Go to the previous page">&larr; prev</a></li>
+    <?php if ($currentPage > 1): ?>
+        <li class="arrow">
+            <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . ($currentPage - 2) * $context->limit ?>" title="Go to the previous page">← prev</a>
+        </li>
+    <?php else: ?>
+        <li class="ellipsis">
+            <span class="disabled" aria-disabled="true" title="Go to the previous page">← prev</span>
+        </li>
     <?php endif; ?>
-    <?php for ($page = 1; $page*$context->limit < $context->total+$context->limit; $page++ ) {
-        $link = $context->url.'&amp;limit='.$context->limit.'&amp;offset='.($page-1)*$context->limit;
-        $class = '';
-        if (($page-1)*$context->limit == $context->offset) {
-            $class = 'selected';
-        }
-    ?>
-    <li class="<?php echo $class; ?>">
-        <?php
-        if ($class !== 'selected') { ?>
-            <a href="<?php echo $link; ?>" title="Go to page <?php echo $page; ?>"><?php echo $page; ?></a>
-        <?php
-        } else {
-            echo $page;
-        } ?>
     </li>
-    <?php } ?>
-    <?php if (($context->offset+$context->limit) < $context->total) :?>
-    <li class="arrow"><a href="<?php echo $context->url.'&amp;limit='.$context->limit.'&amp;offset='.($context->offset+$context->limit); ?>" title="Go to the next page">next &rarr;</a></li>
+
+    <?php if ($totalPages < $pageMinimum + $adjacentPageCount * 2): // not enough pages ?>
+        <?php for ($counter = 1; $counter <= $totalPages; $counter++): ?>
+            <li<?php if ($counter == $currentPage): ?> class="selected"<?php endif;?>>
+                <?php if ($counter == $currentPage): ?>
+                    <?php echo $counter ?>
+                <?php else: ?>
+                    <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . ($counter - 1) * $context->limit ?>"><?php echo $counter ?></a>
+                <?php endif; ?>
+            </li>
+        <?php endfor; ?>
+    <?php else: // enough pages to hide some ?>
+        <?php if ($currentPage < 1 + $adjacentPageCount * 3): // close to the beginning ?>
+            <?php for ($counter = 1; $counter < $pageWindow + $adjacentPageCount * 2; $counter++): ?>
+                <li<?php if ($counter == $currentPage): ?> class="selected"<?php endif;?>>
+                    <?php if ($counter == $currentPage): ?>
+                        <?php echo $counter ?>
+                    <?php elseif ($counter == 1): ?>
+                        <a href="<?php echo $context->url ?>"><?php echo $counter ?></a>
+                    <?php else: ?>
+                        <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . ($counter - 1) * $context->limit ?>"><?php echo $counter ?></a>
+                    <?php endif; ?>
+                </li>
+            <?php endfor; ?>
+            <li class="ellipsis">&hellip;</li>
+            <?php for ($bookendCounter = $pageBookendCount; $bookendCounter > 0; $bookendCounter--): ?>
+                <li>
+                    <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . ($totalPages - $bookendCounter) * $context->limit ?>"><?php echo $totalPages - $bookendCounter + 1 ?></a>
+                </li>
+            <?php endfor; ?>
+        <?php elseif ($totalPages - $adjacentPageCount * 2 > $currentPage && $currentPage > $adjacentPageCount * 2): // in the middle ?>
+            <?php for ($bookendCounter = 0; $bookendCounter < $pageBookendCount; $bookendCounter++): ?>
+                <li>
+                    <?php if (!$bookendCounter): ?>
+                        <a href="<?php echo $context->url ?>"><?php echo $bookendCounter + 1 ?></a>
+                    <?php else: ?>
+                        <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . $bookendCounter * $context->limit ?>"><?php echo $bookendCounter + 1 ?></a>
+                    <?php endif; ?>
+                </li>
+            <?php endfor; ?>
+            <li class="ellipsis">&hellip;</li>
+
+            <?php for ($counter = $currentPage - $adjacentPageCount; $counter <= $currentPage + $adjacentPageCount; $counter++): ?>
+                <li<?php if ($counter == $currentPage): ?> class="selected"<?php endif;?>>
+                    <?php if ($counter == $currentPage): ?>
+                        <?php echo $counter ?>
+                    <?php else: ?>
+                        <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . ($counter - 1) * $context->limit ?>"><?php echo $counter ?></a>
+                    <?php endif; ?>
+                </li>
+            <?php endfor; ?>
+
+            <li class="ellipsis">&hellip;</li>
+            <?php for ($bookendCounter = $pageBookendCount; $bookendCounter > 0; $bookendCounter--): ?>
+                <li>
+                    <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . ($totalPages - $bookendCounter) * $context->limit ?>"><?php echo $totalPages - $bookendCounter + 1 ?></a>
+                </li>
+            <?php endfor; ?>
+        <?php else: // close to end ?>
+            <?php for ($bookendCounter = 0; $bookendCounter < $pageBookendCount; $bookendCounter++): ?>
+                <li>
+                    <?php if (!$bookendCounter): ?>
+                        <a href="<?php echo $context->url ?>"><?php echo $bookendCounter + 1 ?></a>
+                    <?php else: ?>
+                        <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . $bookendCounter * $context->limit ?>"><?php echo $bookendCounter + 1 ?></a>
+                    <?php endif; ?>
+                </li>
+            <?php endfor; ?>
+            <li class="ellipsis">&hellip;</li>
+
+            <?php for ($counter = $totalPages - 1 - $adjacentPageCount * 3; $counter <= $totalPages; $counter++): ?>
+                <li<?php if ($counter == $currentPage): ?> class="selected"<?php endif;?>>
+                    <?php if ($counter == $currentPage): ?>
+                        <?php echo $counter ?>
+                    <?php else: ?>
+                        <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . ($counter - 1) * $context->limit ?>"><?php echo $counter ?></a>
+                    <?php endif; ?>
+                </li>
+            <?php endfor; ?>
+        <?php endif; ?>
     <?php endif; ?>
+
+    <?php if ($currentPage < $counter - 1): ?>
+        <li class="arrow">
+            <a href="<?php echo $context->url . '&amp;limit=' . $context->limit . '&amp;offset=' . ($currentPage * $context->limit); ?>" title="Go to the next page">next →</a>
+        </li>
+    <?php else: ?>
+        <li class="ellipsis">
+            <span class="disabled" aria-disabled="true" title="Go to the next page">next →</span>
+        </li>
+    <?php endif; ?>
+    </li>
 </ul>
