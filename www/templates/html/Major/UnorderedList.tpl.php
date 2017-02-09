7b51f04fd99d5a8fff1d78f0508c3c2c16c86fcb
Kevin Abel <kabel2@unl.edu>
2017-02-09 15:19:15 -0600

    Redesign major list to show cards with icons
    Icons provided by Admissions.
    Card styles adapted from events.

--- www/templates/html/Major/UnorderedList.tpl.php
+++ www/templates/html/Major/UnorderedList.tpl.php
@@ -16,6 +16,10 @@
         $class[] = $college->abbreviation;
     }
     ?>
-    <li class="<?php echo implode(' ', $class); ?>"><a href="<?php echo $major->getRawObject()->getURL(); ?>"><?php echo $major->title; ?><?php if ($note): ?> <span class="note"><?php echo $note; ?></span><?php endif; ?></a></li>
+    <li class="<?php echo implode(' ', $class); ?>"><a href="<?php echo $major->getRawObject()->getURL(); ?>">
+        <span aria-hidden="true" class="major-img"></span>
+        <span class="major-name"><?php echo $major->title; ?></span>
+        <?php if ($note): ?> <span class="note"><?php echo $note; ?></span><?php endif; ?>
+    </a></li>
     <?php endforeach; ?>
 </ul>
